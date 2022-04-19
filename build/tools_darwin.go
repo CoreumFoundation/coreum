8b962199e4b2317554a41fb6e838c86f6568e331
Wojtek <103407812+wojtek-coreum@users.noreply.github.com>
2022-04-19 14:47:43 +0200

    Install ignite (#10)

--- build/tools_darwin.go
+++ build/tools_darwin.go
@@ -17,4 +17,12 @@ var tools = map[string]tool{
 			"golangci-lint-1.45.2-darwin-amd64/golangci-lint",
 		},
 	},
+	"ignite": {
+		Version: "v0.20.4",
+		URL:     "https://github.com/ignite-hq/cli/releases/download/v0.20.4/ignite_0.20.4_darwin_amd64.tar.gz",
+		Hash:    "sha256:2e9366168de8b8dbf743ec0de21c93430eca79c76d947c6de4d7c728c757f05e",
+		Binaries: []string{
+			"ignite",
+		},
+	},
 }
