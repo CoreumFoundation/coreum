f3970aa2426deafcf17b1032c79ec0e2044b2627
Wojtek <wojtek@exw.co>
2022-05-16 17:45:36 +0200

    Sort `build` actions

--- build/index.go
+++ build/index.go
@@ -2,11 +2,11 @@ package build
 
 // Commands is a definition of commands available in build system
 var Commands = map[string]interface{}{
-	"setup":          installTools,
-	"lint":           goLint,
-	"tidy":           goModTidy,
-	"test":           goTest,
 	"build":          buildAll,
-	"build/coreznet": buildCoreZNet,
 	"build/cored":    buildCored,
+	"build/coreznet": buildCoreZNet,
+	"lint":           goLint,
+	"setup":          installTools,
+	"test":           goTest,
+	"tidy":           goModTidy,
 }
