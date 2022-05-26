8497a30f87e08d1fde0e6c37ae0e621505ff4c70
Wojtek <wojtek@coreum.com>
2022-05-26 08:11:04 +0200

    Move client to separate file (code stays the same)

--- coreznet/infra/apps/cored/types.go
+++ coreznet/infra/apps/cored/types.go
@@ -1,11 +1,8 @@
 package cored
 
 import (
-	"context"
-	"encoding/json"
 	"fmt"
 	"math/big"
-	"net"
 )
 
 // Ports defines ports used by cored application
@@ -41,63 +38,4 @@ type Balance struct {
 
 func (b Balance) String() string {
 	return b.Amount.String() + b.Denom
-}
-
-// NewClient creates new client for cored
-func NewClient(executor Executor, ip net.IP, rpcPort int) *Client {
-	return &Client{
-		executor: executor,
-		ip:       ip,
-		rpcPort:  rpcPort,
-	}
-}
-
-// Client is the client for cored blockchain
-type Client struct {
-	executor Executor
-	ip       net.IP
-	rpcPort  int
-}
-
-// QBankBalances queries for bank balances owned by wallet
-func (c *Client) QBankBalances(ctx context.Context, wallet Wallet) (map[string]Balance, error) {
-	// FIXME (wojtek): support pagination
-	out, err := c.executor.QBankBalances(ctx, wallet.Address, c.ip, c.rpcPort)
-	if err != nil {
-		return nil, err
-	}
-	data := struct {
-		Balances []struct {
-			Amount string `json:"amount"`
-			Denom  string `json:"denom"`
-		} `json:"balances"`
-	}{}
-	if err := json.Unmarshal(out, &data); err != nil {
-		return nil, err
-	}
-
-	balances := map[string]Balance{}
-	for _, b := range data.Balances {
-		amount, ok := big.NewInt(0).SetString(b.Amount, 10)
-		if !ok {
-			panic(fmt.Sprintf("invalid amount %s received for denom %s on wallet %s", b.Amount, b.Denom, wallet.Address))
-		}
-		balances[b.Denom] = Balance{Amount: amount, Denom: b.Denom}
-	}
-	return balances, nil
-}
-
-// TxBankSend sends tokens from one wallet to another
-func (c *Client) TxBankSend(ctx context.Context, sender, receiver Wallet, balance Balance) (string, error) {
-	out, err := c.executor.TxBankSend(ctx, sender.Name, receiver.Address, balance, c.ip, c.rpcPort)
-	if err != nil {
-		return "", err
-	}
-	data := struct {
-		TxHash string `json:"txhash"`
-	}{}
-	if err := json.Unmarshal(out, &data); err != nil {
-		return "", err
-	}
-	return data.TxHash, nil
 }
