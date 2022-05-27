4b0cc706f482fde4e0f6c7238c00a2522bc92fe4
Wojtek <wojtek@coreum.com>
2022-05-27 09:13:36 +0200

    Skeleton of `corezstress` command

--- coreznet/cmd/corezstress/main.go
+++ coreznet/cmd/corezstress/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"github.com/CoreumFoundation/coreum-tools/pkg/run"
+	"github.com/spf13/cobra"
+
+	"github.com/CoreumFoundation/coreum/coreznet/cmd"
+	"github.com/CoreumFoundation/coreum/coreznet/pkg/zstress"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	cmd.ConfigureLoggerWithCLI(false)
+	run.Tool("corezstress", nil, func() error {
+		rootCmd := &cobra.Command{
+			SilenceUsage: true,
+			Short:        "Run benchmark test",
+			RunE: func(cmd *cobra.Command, args []string) error {
+				panic("not implemented")
+			},
+		}
+		// dummy flag to match the one added by logger configurator
+		rootCmd.PersistentFlags().BoolP("verbose", "v", false, "Turns on verbose logging")
+
+		generateCmd := &cobra.Command{
+			Use:   "generate",
+			Short: "Generates all the files required to deploy the blockchain used for benchmarking",
+			RunE: func(cmd *cobra.Command, args []string) error {
+				return zstress.Generate()
+			},
+		}
+		rootCmd.AddCommand(generateCmd)
+
+		return rootCmd.Execute()
+	})
 }
---
+++ coreznet/pkg/zstress/generate.go
@@ -0,0 +1,6 @@
+package zstress
+
+// Generate generates all the files required to deploy blockchain used for benchmarking
+func Generate() error {
+	panic("not implemented")
+}
