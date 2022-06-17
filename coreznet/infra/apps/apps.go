e1afc43ac1c62762b45bc493e58d9fd3770ca88c
Wojtek <wojtek@coreum.com>
2022-06-17 09:32:40 +0200

    `coreznet`: Add function to create network of cored nodes

--- coreznet/infra/apps/apps.go
+++ coreznet/infra/apps/apps.go
@@ -1,6 +1,8 @@
 package apps
 
 import (
+	"fmt"
+
 	"github.com/CoreumFoundation/coreum/coreznet/infra"
 	"github.com/CoreumFoundation/coreum/coreznet/infra/apps/cored"
 )
@@ -19,7 +21,26 @@ type Factory struct {
 	spec   *infra.Spec
 }
 
-// Cored creates new cored app
-func (f *Factory) Cored(name string, ports cored.Ports, genesis *cored.Genesis, rootNode *Cored) Cored {
-	return NewCored(name, f.config, genesis, f.spec.DescribeApp(CoredType, name), ports, rootNode)
+// CoredNetwork creates new network of cored nodes
+func (f *Factory) CoredNetwork(name string, numOfNodes int) infra.Mode {
+	genesis := cored.NewGenesis(name)
+	nodes := make(infra.Mode, 0, numOfNodes)
+	var node0 *Cored
+	for i := 0; i < numOfNodes; i++ {
+		port := 10000 + 10*i
+		name := name + fmt.Sprintf("-%02d", i)
+		node := NewCored(name, f.config, genesis, f.spec.DescribeApp(CoredType, name), cored.Ports{
+			RPC:        port + 1,
+			P2P:        port + 2,
+			GRPC:       port + 3,
+			GRPCWeb:    port + 4,
+			PProf:      port + 5,
+			Prometheus: port + 6,
+		}, node0)
+		if node0 == nil {
+			node0 = &node
+		}
+		nodes = append(nodes, node)
+	}
+	return nodes
 }
--- coreznet/pkg/znet/mode.go
+++ coreznet/pkg/znet/mode.go
@@ -3,19 +3,16 @@ package znet
 import (
 	"github.com/CoreumFoundation/coreum/coreznet/infra"
 	"github.com/CoreumFoundation/coreum/coreznet/infra/apps"
-	"github.com/CoreumFoundation/coreum/coreznet/infra/apps/cored"
 	"github.com/CoreumFoundation/coreum/coreznet/tests"
 )
 
 // DevMode is the environment for developer
-func DevMode(af *apps.Factory) infra.Mode {
-	return infra.Mode{
-		af.Cored("cored-node", cored.DefaultPorts, cored.NewGenesis("coredev"), nil),
-	}
+func DevMode(appF *apps.Factory) infra.Mode {
+	return appF.CoredNetwork("coredev", 1)
 }
 
 // TestMode returns environment used for testing
-func TestMode(af *apps.Factory) infra.Mode {
-	env, _ := tests.Tests(af)
+func TestMode(appF *apps.Factory) infra.Mode {
+	env, _ := tests.Tests(appF)
 	return env
 }
