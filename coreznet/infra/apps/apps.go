42225bfe539302775818dceec68b99b5668b770c
Wojtek <103407812+wojtek-coreum@users.noreply.github.com>
2022-06-17 14:51:18 +0200

    `coreznet`: Add function to create network of cored nodes (#65)
    * `coreznet`: Add function to create network of cored nodes
    
    * Define ports in more predictable way, related to default ports
    
    * Increment ports by 100, not 1000

--- coreznet/infra/apps/apps.go
+++ coreznet/infra/apps/apps.go
@@ -1,6 +1,8 @@
 package apps
 
 import (
+	"fmt"
+
 	"github.com/CoreumFoundation/coreum/coreznet/infra"
 	"github.com/CoreumFoundation/coreum/coreznet/infra/apps/cored"
 )
@@ -19,7 +21,26 @@ type Factory struct {
 	spec   *infra.Spec
 }
 
-// Cored creates new cored app
-func (f *Factory) Cored(name string, ports cored.Ports, genesis *cored.Genesis, rootNode *Cored) Cored {
-	return NewCored(name, f.config, genesis, f.spec.DescribeApp(CoredType, name), ports, rootNode)
+// CoredNetwork creates new network of cored nodes
+func (f *Factory) CoredNetwork(name string, numOfNodes int) infra.Mode {
+	genesis := cored.NewGenesis(name)
+	nodes := make(infra.Mode, 0, numOfNodes)
+	var node0 *Cored
+	for i := 0; i < numOfNodes; i++ {
+		name := name + fmt.Sprintf("-%02d", i)
+		portDelta := i * 100
+		node := NewCored(name, f.config, genesis, f.spec.DescribeApp(CoredType, name), cored.Ports{
+			RPC:        cored.DefaultPorts.RPC + portDelta,
+			P2P:        cored.DefaultPorts.P2P + portDelta,
+			GRPC:       cored.DefaultPorts.GRPC + portDelta,
+			GRPCWeb:    cored.DefaultPorts.GRPCWeb + portDelta,
+			PProf:      cored.DefaultPorts.PProf + portDelta,
+			Prometheus: cored.DefaultPorts.Prometheus + portDelta,
+		}, node0)
+		if node0 == nil {
+			node0 = &node
+		}
+		nodes = append(nodes, node)
+	}
+	return nodes
 }
