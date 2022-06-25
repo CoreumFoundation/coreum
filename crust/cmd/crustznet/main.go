5018f4d52a8c39af7e419437a4663e3267a3a45c
Wojtek <wojtek@coreum.com>
2022-06-25 11:40:16 +0200

    Stop and remove applications based on `mode` and `target` stored in spec file

--- crust/cmd/crustznet/main.go
+++ crust/cmd/crustznet/main.go
@@ -16,7 +16,7 @@ import (
 )
 
 func main() {
-	run.Tool("crustznet", znet.IoC, func(c *ioc.Container, configF *znet.ConfigFactory, cmdF *znet.CmdFactory) error {
+	run.Tool("crustznet", znet.IoC, func(c *ioc.Container, configF *infra.ConfigFactory, cmdF *znet.CmdFactory) error {
 		rootCmd := &cobra.Command{
 			SilenceUsage:  true,
 			SilenceErrors: true,
@@ -25,9 +25,9 @@ func main() {
 		}
 		logger.AddFlags(logger.ToolDefaultConfig, rootCmd.PersistentFlags())
 		rootCmd.PersistentFlags().StringVar(&configF.EnvName, "env", defaultString("CRUSTZNET_ENV", "crustznet"), "Name of the environment to run in")
-		rootCmd.PersistentFlags().StringVar(&configF.Target, "target", defaultString("CRUSTZNET_TARGET", "docker"), "Target of the deployment: "+strings.Join(c.Names((*infra.Target)(nil)), " | "))
 		rootCmd.PersistentFlags().StringVar(&configF.HomeDir, "home", defaultString("CRUSTZNET_HOME", must.String(os.UserCacheDir())+"/crustznet"), "Directory where all files created automatically by crustznet are stored")
-		addFlags(rootCmd, configF)
+		addBinDirFlag(rootCmd, configF)
+		addTargetFlag(rootCmd, c, configF)
 		addModeFlag(rootCmd, c, configF)
 		addFilterFlag(rootCmd, configF)
 
@@ -36,70 +36,68 @@ func main() {
 			Short: "Starts environment",
 			RunE:  cmdF.Cmd(znet.Start),
 		}
-		addFlags(startCmd, configF)
+		addBinDirFlag(startCmd, configF)
+		addTargetFlag(startCmd, c, configF)
 		addModeFlag(startCmd, c, configF)
 		rootCmd.AddCommand(startCmd)
 
-		stopCmd := &cobra.Command{
+		rootCmd.AddCommand(&cobra.Command{
 			Use:   "stop",
 			Short: "Stops environment",
 			RunE:  cmdF.Cmd(znet.Stop),
-		}
-		rootCmd.AddCommand(stopCmd)
+		})
 
-		removeCmd := &cobra.Command{
+		rootCmd.AddCommand(&cobra.Command{
 			Use:   "remove",
 			Short: "Removes environment",
 			RunE:  cmdF.Cmd(znet.Remove),
-		}
-		rootCmd.AddCommand(removeCmd)
+		})
 
 		testCmd := &cobra.Command{
 			Use:   "test",
 			Short: "Runs integration tests",
 			RunE:  cmdF.Cmd(znet.Test),
 		}
-		addFlags(testCmd, configF)
+		addBinDirFlag(testCmd, configF)
+		addTargetFlag(testCmd, c, configF)
 		addFilterFlag(testCmd, configF)
 		rootCmd.AddCommand(testCmd)
 
-		specCmd := &cobra.Command{
+		rootCmd.AddCommand(&cobra.Command{
 			Use:   "spec",
 			Short: "Prints specification of running environment",
 			RunE:  cmdF.Cmd(znet.Spec),
-		}
-		addModeFlag(specCmd, c, configF)
-		rootCmd.AddCommand(specCmd)
+		})
 
-		pingPongCmd := &cobra.Command{
+		rootCmd.AddCommand(&cobra.Command{
 			Use:   "ping-pong",
 			Short: "Sends tokens back and forth to generate transactions",
 			RunE:  cmdF.Cmd(znet.PingPong),
-		}
-		addModeFlag(pingPongCmd, c, configF)
-		rootCmd.AddCommand(pingPongCmd)
+		})
 
-		stressCmd := &cobra.Command{
+		rootCmd.AddCommand(&cobra.Command{
 			Use:   "stress",
 			Short: "Runs the logic used by crustzstress to test benchmarking",
 			RunE:  cmdF.Cmd(znet.Stress),
-		}
-		addModeFlag(stressCmd, c, configF)
-		rootCmd.AddCommand(stressCmd)
+		})
 
 		return rootCmd.Execute()
 	})
 }
 
-func addFlags(cmd *cobra.Command, configF *znet.ConfigFactory) {
+func addBinDirFlag(cmd *cobra.Command, configF *infra.ConfigFactory) {
 	cmd.Flags().StringVar(&configF.BinDir, "bin-dir", defaultString("CRUSTZNET_BIN_DIR", filepath.Dir(must.String(filepath.EvalSymlinks(must.String(os.Executable()))))), "Path to directory where executables exist")
 }
 
-func addModeFlag(cmd *cobra.Command, c *ioc.Container, configF *znet.ConfigFactory) {
+func addTargetFlag(cmd *cobra.Command, c *ioc.Container, configF *infra.ConfigFactory) {
+	cmd.Flags().StringVar(&configF.Target, "target", defaultString("CRUSTZNET_TARGET", "docker"), "Target of the deployment: "+strings.Join(c.Names((*infra.Target)(nil)), " | "))
+}
+
+func addModeFlag(cmd *cobra.Command, c *ioc.Container, configF *infra.ConfigFactory) {
 	cmd.Flags().StringVar(&configF.ModeName, "mode", defaultString("CRUSTZNET_MODE", "dev"), "List of applications to deploy: "+strings.Join(c.Names((*infra.Mode)(nil)), " | "))
 }
 
-func addFilterFlag(cmd *cobra.Command, configF *znet.ConfigFactory) {
+func addFilterFlag(cmd *cobra.Command, configF *infra.ConfigFactory) {
 	cmd.Flags().StringArrayVar(&configF.TestFilters, "filter", defaultFilters("CRUSTZNET_FILTERS"), "Regular expression used to filter tests to run")
 }
 
