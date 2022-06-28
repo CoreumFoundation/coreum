634a8e0979ee8d2b58f8ff3be55e0ed1e3e7b9fc
Milad Zahedi <mza1989@gmail.com>
2022-06-28 16:58:00 +0430

    addressed comments

--- cored/pkg/config/genesis_test.go
+++ cored/pkg/config/genesis_test.go
@@ -12,34 +12,34 @@ import (
 )
 
 func TestGenesisValidation(t *testing.T) {
-	assert := assert.New(t)
-	require := require.New(t)
+	assertion := assert.New(t)
+	required := require.New(t)
 	dirPath, err := ioutil.TempDir("", "genesis_test")
-	require.NoError(err)
-	defer require.NoError(os.RemoveAll(dirPath))
+	required.NoError(err)
+	defer required.NoError(os.RemoveAll(dirPath))
 
 	n, err := NetworkByChainID(string(Devnet))
-	require.NoError(err)
+	required.NoError(err)
 
 	gen, err := n.Genesis()
-	require.NoError(err)
-	require.NoError(gen.Save(dirPath))
+	required.NoError(err)
+	required.NoError(gen.Save(dirPath))
 
 	parsedGenesisDoc, err := tmtypes.GenesisDocFromFile(dirPath + "/config/genesis.json")
-	require.NoError(err)
-	assert.EqualValues(parsedGenesisDoc.ChainID, gen.genesisDoc.ChainID)
-	assert.EqualValues(parsedGenesisDoc.ConsensusParams, gen.genesisDoc.ConsensusParams)
-	assert.EqualValues(parsedGenesisDoc.GenesisTime, gen.genesisDoc.GenesisTime)
-	assert.EqualValues(parsedGenesisDoc.InitialHeight, gen.genesisDoc.InitialHeight)
-	assert.EqualValues(parsedGenesisDoc.Validators, gen.genesisDoc.Validators)
+	required.NoError(err)
+	assertion.EqualValues(parsedGenesisDoc.ChainID, gen.genesisDoc.ChainID)
+	assertion.EqualValues(parsedGenesisDoc.ConsensusParams, gen.genesisDoc.ConsensusParams)
+	assertion.EqualValues(parsedGenesisDoc.GenesisTime, gen.genesisDoc.GenesisTime)
+	assertion.EqualValues(parsedGenesisDoc.InitialHeight, gen.genesisDoc.InitialHeight)
+	assertion.EqualValues(parsedGenesisDoc.Validators, gen.genesisDoc.Validators)
 
 	// In order to compare app state, we need to unmarshal it first
 	// because comparing json.RawMessage may give false negatives.
 	appStateMap := map[string]interface{}{}
 	err = json.Unmarshal(gen.genesisDoc.AppState, &appStateMap)
-	assert.NoError(err)
+	assertion.NoError(err)
 	parsedAppStateMap := map[string]interface{}{}
 	err = json.Unmarshal(parsedGenesisDoc.AppState, &parsedAppStateMap)
-	assert.NoError(err)
-	assert.EqualValues(appStateMap, parsedAppStateMap)
+	assertion.NoError(err)
+	assertion.EqualValues(appStateMap, parsedAppStateMap)
 }
