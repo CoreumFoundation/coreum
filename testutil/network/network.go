e9da36a508f93bc1ec85f467d637907828851782
Wojtek <wojtek@coreum.com>
2022-06-29 15:54:09 +0200

    Move `cored` code to root directory
    Usage:
      cored [command]
    
    Available Commands:
      add-genesis-account Add a genesis account to genesis.json
      collect-gentxs      Collect genesis txs and output a genesis.json file
      config              Create or query an application CLI configuration file
      debug               Tool for helping with debugging your application
      export              Export state to JSON
      gentx               Generate a genesis tx carrying a self delegation
      help                Help about any command
      init                Initialize private validator, p2p, genesis, and application configuration files
      keys                Manage your application's keys
      migrate             Migrate genesis to a specified target version
      query               Querying subcommands
      rollback            rollback cosmos-sdk and tendermint state by one height
      start               Run the full node
      status              Query remote node for status
      tendermint          Tendermint subcommands
      tx                  Transactions subcommands
      validate-genesis    validates the genesis file at the default location or at the location passed as an arg
      version             Print the application binary version information
    
    Flags:
      -h, --help                help for cored
          --home string         directory for config and data (default "/home/wojciech/.core")
          --log_format string   The logging format (json|plain) (default "plain")
          --log_level string    The logging level (trace|debug|info|warn|error|fatal|panic) (default "info")
          --trace               print out full stack trace on errors
    
    Use "cored [command] --help" for more information about a command. node to root directory

--- testutil/network/network.go
+++ testutil/network/network.go
@@ -18,7 +18,7 @@ import (
 	tmrand "github.com/tendermint/tendermint/libs/rand"
 	tmdb "github.com/tendermint/tm-db"
 
-	"github.com/CoreumFoundation/coreum/cored/app"
+	"github.com/CoreumFoundation/coreum/app"
 )
 
 type (
