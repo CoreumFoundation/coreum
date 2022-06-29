3d28ee7ed04126a7eb9933442b37045b878447dc
Wojtek <103407812+wojtek-coreum@users.noreply.github.com>
2022-06-29 15:46:22 +0200

    Adjust CI to use public `crust` repo and remove non-`cored` code (#102)
    * Adjust build system to use public `crust` repo
    
    * Remove non-`cored` stuff
    
    * Merge master into use-crust

--- crust/infra/apps/blockexplorer/hasura.go
+++
@@ -1,9 +0,0 @@
-package blockexplorer
-
-import (
-	_ "embed"
-)
-
-// HasuraMetadataTemplate contains hasura metadata template
-//go:embed hasura/metadata/metadata.tmpl.json
-var HasuraMetadataTemplate string
