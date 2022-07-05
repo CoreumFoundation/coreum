6d7387c8599d152c45f05f1eb7634bf6d4b93515
milad <mza1989@gmail.com>
2022-07-05 15:24:48 +0430

    copied network config code from crust to cored folder (#105)
    * copied network config code from crust to cored folder
    
    this PR is the first of a set of PRs intended to create
    config for different networks. here we create a package
    which holds the configurations and in the next 2 PRs. we
    will use them in cored cli and crust package.
    
    * satisfy linter
    
    * changed default net to mainnet
    
    * addressed PR comments
    
    * fixed PR comments
    
    * added test, addressed more comments
    
    * addressed PR comments
    
    * satisfy linter
    
    * addressed comments
    
    * addressed PR comments
    
    * pass linter
    
    * moved pkg folder to the root
    
    * Addressed PR comments
    
    * Addressed PR comments
    
    * pass linter
    
    * addressed PR comments
    
    * moved set prefixes to init function in tests
    
    * Addressed PR Comments
    
    * removed line for mutex initialization

---
+++ pkg/types/types.go
@@ -0,0 +1,37 @@
+package types
+
+import (
+	"math/big"
+)
+
+// Wallet stores information related to wallet
+type Wallet struct {
+	// Name is the name of the key stored in keystore
+	Name string
+
+	// Key is the private key of the wallet
+	Key Secp256k1PrivateKey
+
+	// AccountNumber is the account number as stored on blockchain
+	AccountNumber uint64
+
+	// AccountSequence is the sequence of next transaction to sign
+	AccountSequence uint64
+}
+
+func (w Wallet) String() string {
+	return w.Name + "@" + w.Key.Address()
+}
+
+// Balance stores balance of denom
+type Balance struct {
+	// Amount is stored amount
+	Amount *big.Int `json:"amount"`
+
+	// Denom is a token symbol
+	Denom string `json:"denom"`
+}
+
+func (b Balance) String() string {
+	return b.Amount.String() + b.Denom
+}
