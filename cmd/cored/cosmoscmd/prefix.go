fad8e1b5085841db6bb2a63c52b9d77e88a2f39e
Milad Zahedi <mza1989@gmail.com>
2022-07-12 14:37:20 +0430

    Copied cosmoscmd code from ignite-hq/cli repository

---
+++ cmd/cored/cosmoscmd/prefix.go
@@ -0,0 +1,21 @@
+// COPIED FROM https://github.com/ignite/cli/tree/e6a5efdaa2210fb72e33382d442268cdd466ae2d/ignite/pkg/cosmoscmd
+// UNDER APACHE2.0 LICENSE
+package cosmoscmd
+
+import sdk "github.com/cosmos/cosmos-sdk/types"
+
+func SetPrefixes(accountAddressPrefix string) {
+	// Set prefixes
+	accountPubKeyPrefix := accountAddressPrefix + "pub"
+	validatorAddressPrefix := accountAddressPrefix + "valoper"
+	validatorPubKeyPrefix := accountAddressPrefix + "valoperpub"
+	consNodeAddressPrefix := accountAddressPrefix + "valcons"
+	consNodePubKeyPrefix := accountAddressPrefix + "valconspub"
+
+	// Set and seal config
+	config := sdk.GetConfig()
+	config.SetBech32PrefixForAccount(accountAddressPrefix, accountPubKeyPrefix)
+	config.SetBech32PrefixForValidator(validatorAddressPrefix, validatorPubKeyPrefix)
+	config.SetBech32PrefixForConsensusNode(consNodeAddressPrefix, consNodePubKeyPrefix)
+	config.Seal()
+}
