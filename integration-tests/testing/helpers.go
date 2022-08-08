f650cac5908a1fe7ea454e3426d235cfb514ad3b
Wojtek <wojtek@coreum.com>
2022-08-08 09:41:09 +0200

    Compute initial balances in integration tests by taking gas limits and prices from network

--- integration-tests/testing/helpers.go
+++ integration-tests/testing/helpers.go
@@ -1,6 +1,8 @@
 package testing
 
 import (
+	"math/big"
+
 	"github.com/CoreumFoundation/coreum/pkg/types"
 )
 
@@ -8,4 +10,12 @@ import (
 func RandomWallet() types.Wallet {
 	_, privKey := types.GenerateSecp256k1Key()
 	return types.Wallet{Name: privKey.Address(), Key: privKey}
+}
+
+// ComputeInitialBalance computes initial balance by multiplying gas price by deterministic amount of gas required by single transaction and number of expected transactions
+// plus additional balance for transfers
+func ComputeInitialBalance(gasPrice *big.Int, messageGasLimit uint64, numOfMessages int, freeAmount *big.Int) *big.Int {
+	initialBalance := new(big.Int).Mul(gasPrice, big.NewInt(int64(messageGasLimit)))
+	initialBalance.Mul(initialBalance, big.NewInt(int64(numOfMessages)))
+	return initialBalance.Add(initialBalance, freeAmount)
 }
