506ba120d11d23a4143bfcf3603625c3fa054561
Wojtek <wojtek@coreum.com>
2022-08-26 09:09:14 +0200

    Fuzz testing of fee model

--- x/feemodel/model_test.go
+++ x/feemodel/model_test.go
@@ -1,6 +1,9 @@
 package feemodel
 
 import (
+	"math"
+	"math/rand"
+	"strconv"
 	"testing"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -99,4 +102,83 @@ func TestShapeInEscalationRegion(t *testing.T) {
 
 		lastPrice = nextPrice
 	}
+}
+
+func TestFuzzGasPriceInExpectedRange(t *testing.T) {
+	t.Parallel()
+
+	for i := 0; i < 100; i++ {
+		t.Run("RandomCase", func(t *testing.T) {
+			t.Parallel()
+
+			model, shortEMA, longEMA := generateRandomizedModel()
+			logParameters(t, model, shortEMA, longEMA)
+			nextGasPrice := model.CalculateNextGasPrice(shortEMA, longEMA)
+
+			assert.True(t, nextGasPrice.GT(sdk.ZeroInt()))
+			assert.True(t, nextGasPrice.LTE(model.MaxGasPrice))
+
+			switch {
+			case shortEMA >= model.MaxBlockGas:
+				assert.True(t, nextGasPrice.Equal(model.MaxGasPrice))
+			case shortEMA > model.EscalationStartBlockGas:
+				assert.True(t, nextGasPrice.LT(model.MaxGasPrice))
+				assert.True(t, nextGasPrice.GTE(model.computeGasPriceWithMaxDiscount()))
+			case shortEMA >= longEMA:
+				assert.True(t, nextGasPrice.Equal(model.computeGasPriceWithMaxDiscount()))
+			case shortEMA > 0:
+				assert.True(t, nextGasPrice.LT(model.InitialGasPrice))
+			default:
+				assert.True(t, nextGasPrice.Equal(model.InitialGasPrice))
+			}
+		})
+	}
+}
+
+func generateRandomizedModel() (model Model, shortEMA, longEMA int64) {
+	// fuzz engine of go test is not used because it doesn't allow to maintain relationships between generated parameters
+
+	initialGasPrice := rand.Uint64()/2 + 10
+	maxGasPrice := initialGasPrice + uint64(float64(math.MaxUint64-initialGasPrice-10)*rand.Float64()+10.0)
+	shortAverageBlockLength := uint(rand.Intn(1000) + 1)
+	longAverageBlockLength := shortAverageBlockLength + uint(rand.Intn(10000))
+	maxBlockGas := rand.Int63()
+	escalationStartBlockGas := rand.Int63n(maxBlockGas-2) + 1
+	// rand.Float64() returns random number in [0.0, 1.0), but we want (0.0, 1.0],
+	// calculating 1.0 - rand.Float64() is the simplest way of achieving this.
+	maxDiscount := (1.0 - rand.Float64()) * 0.8 // this gives a number in range (0.0, 0.8]
+
+	shortEMA = rand.Int63()
+	longEMA = rand.Int63()
+
+	return Model{
+		InitialGasPrice:         sdk.NewIntFromUint64(initialGasPrice),
+		MaxGasPrice:             sdk.NewIntFromUint64(maxGasPrice),
+		MaxDiscount:             sdk.MustNewDecFromStr(strconv.FormatFloat(maxDiscount, 'f', 4, 64)),
+		EscalationStartBlockGas: escalationStartBlockGas,
+		MaxBlockGas:             maxBlockGas,
+		ShortAverageBlockLength: shortAverageBlockLength,
+		LongAverageBlockLength:  longAverageBlockLength,
+	}, shortEMA, longEMA
+}
+
+func logParameters(t *testing.T, model Model, shortEMA, longEMA int64) {
+	t.Logf(`InitialGasPrice: %s
+MaxGasPrice: %s
+MaxDiscount: %s
+EscalationStartBlockGas: %d
+MaxBlockGas: %d
+ShortAverageBlockLength: %d
+LongAverageBlockLength: %d
+ShortEMA: %d
+LongEMA: %d`,
+		model.InitialGasPrice,
+		model.MaxGasPrice,
+		model.MaxDiscount,
+		model.EscalationStartBlockGas,
+		model.MaxBlockGas,
+		model.ShortAverageBlockLength,
+		model.LongAverageBlockLength,
+		shortEMA,
+		longEMA)
 }
