ba6000ed23eb20c56f4d2d000208d3c3e1446d37
Dzmitry Hil <dzmitryhil@gmail.com>
2022-08-30 11:47:10 +0300

    Integrate WASM CountTXDecorator into the app ante handler.

--- x/auth/ante/ante.go
+++ x/auth/ante/ante.go
@@ -4,6 +4,8 @@
 package ante
 
 import (
+	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
+	wasmTypes "github.com/CosmWasm/wasmd/x/wasm/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/types/tx/signing"
@@ -16,13 +18,15 @@ import (
 
 // HandlerOptions are the options required for constructing a default SDK AnteHandler.
 type HandlerOptions struct {
-	AccountKeeper   authante.AccountKeeper
-	BankKeeper      types.BankKeeper
-	FeegrantKeeper  authante.FeegrantKeeper
-	FeeModelKeeper  feemodelante.Keeper
-	SignModeHandler authsigning.SignModeHandler
-	SigGasConsumer  func(meter sdk.GasMeter, sig signing.SignatureV2, params types.Params) error
-	GasRequirements DeterministicGasRequirements
+	AccountKeeper         authante.AccountKeeper
+	BankKeeper            types.BankKeeper
+	FeegrantKeeper        authante.FeegrantKeeper
+	FeeModelKeeper        feemodelante.Keeper
+	SignModeHandler       authsigning.SignModeHandler
+	SigGasConsumer        func(meter sdk.GasMeter, sig signing.SignatureV2, params types.Params) error
+	GasRequirements       DeterministicGasRequirements
+	WasmConfig            *wasmTypes.WasmConfig
+	WasmTXCounterStoreKey sdk.StoreKey
 }
 
 // NewAnteHandler returns an AnteHandler that checks and increments sequence
@@ -49,8 +53,17 @@ func NewAnteHandler(options HandlerOptions) (sdk.AnteHandler, error) {
 		options.SigGasConsumer = authante.DefaultSigVerificationGasConsumer
 	}
 
+	if options.WasmConfig == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "wasm config is required for ante builder")
+	}
+
+	if options.WasmTXCounterStoreKey == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "tx counter key is required for ante builder")
+	}
+
 	anteDecorators := []sdk.AnteDecorator{
 		authante.NewSetUpContextDecorator(), // outermost AnteDecorator. SetUpContext must be called first
+		wasmkeeper.NewCountTXDecorator(options.WasmTXCounterStoreKey),
 		authante.NewRejectExtensionOptionsDecorator(),
 		authante.NewValidateBasicDecorator(),
 		authante.NewTxTimeoutHeightDecorator(),
