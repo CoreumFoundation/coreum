9c3f8832e60318bb9dd02c446dacbe531372baf9
Dzmitry Hil <dzmitryhil@gmail.com>
2022-08-31 14:27:40 +0300

    * resolve the merge conflicts

--- integration-tests/wasm/bank_send.go
+++ integration-tests/wasm/bank_send.go
@@ -4,9 +4,8 @@ import (
 	"context"
 	_ "embed"
 	"encoding/json"
-	"math/big"
-
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/stretchr/testify/require"
 
@@ -38,87 +37,83 @@ const (
 
 // TestBankSendWasmContract runs a contract deployment flow and tests that the contract is able to use Bank module
 // to disperse the native coins.
-func TestBankSendWasmContract(chain testing.Chain) (testing.PrepareFunc, testing.RunFunc) {
+func TestBankSendWasmContract(ctx context.Context, t testing.T, chain testing.Chain) {
 	adminWallet := testing.RandomWallet()
-	nativeDenom := chain.Network.TokenSymbol()
+	nativeDenom := chain.NetworkConfig.TokenSymbol
 
-	initTestState := func(ctx context.Context) error {
-		// FIXME (wojtek): Temporary code for transition
-		if chain.Fund != nil {
-			chain.Fund(adminWallet, types.NewCoinUnsafe(big.NewInt(5000000000), chain.Network.TokenSymbol()))
-		}
-		return nil
-	}
+	requireT := require.New(t)
+	requireT.NoError(chain.Faucet.FundAccounts(ctx,
+		testing.FundedAccount{
+			Wallet: adminWallet,
+			Amount: testing.MustNewCoin(t, sdk.NewInt(5000000000), nativeDenom),
+		},
+	))
 
-	runTestFunc := func(ctx context.Context, t testing.T) {
-		requireT := require.New(t)
-		wasmTestClient := newWasmTestClient(tx.BaseInput{
-			Signer:   adminWallet,
-			GasPrice: types.NewCoinUnsafe(chain.Network.FeeModel().InitialGasPrice.BigInt(), nativeDenom),
-		}, chain.Client)
+	wasmTestClient := newWasmTestClient(tx.BaseInput{
+		Signer:   adminWallet,
+		GasPrice: testing.MustNewCoin(t, chain.NetworkConfig.Fee.FeeModel.InitialGasPrice, nativeDenom),
+	}, chain.Client)
 
-		initialPayload, err := json.Marshal(bankInstantiatePayload{
-			Count: 0,
+	initialPayload, err := json.Marshal(bankInstantiatePayload{
+		Count: 0,
+	})
+	requireT.NoError(err)
+	contractAddr, err := wasmTestClient.deployAndInstantiate(
+		ctx,
+		bankSendWASM,
+		instantiateConfig{
+			accessType: wasmtypes.AccessTypeUnspecified,
+			payload:    initialPayload,
+			// transfer some coins during instantiation, so we could withdraw them later using contract code.
+			amount: testing.MustNewCoin(t, sdk.NewInt(10000), nativeDenom),
+			label:  "bank_send",
+		},
+	)
+	requireT.NoError(err)
+
+	contractBalance, err := chain.Client.BankQueryClient().Balance(ctx,
+		&banktypes.QueryBalanceRequest{
+			Address: contractAddr,
+			Denom:   nativeDenom,
 		})
-		requireT.NoError(err)
-		contractAddr, err := wasmTestClient.deployAndInstantiate(
-			ctx,
-			bankSendWASM,
-			instantiateConfig{
-				accessType: wasmtypes.AccessTypeUnspecified,
-				payload:    initialPayload,
-				// transfer some coins during instantiation, so we could withdraw them later using contract code.
-				amount: types.NewCoinUnsafe(big.NewInt(10000), nativeDenom),
-				label:  "bank_send",
-			},
-		)
-		requireT.NoError(err)
+	requireT.NoError(err)
+	requireT.NotNil(contractBalance.Balance)
+	requireT.Equal(nativeDenom, contractBalance.Balance.Denom)
+	requireT.Equal("10000", contractBalance.Balance.Amount.String())
 
-		contractBalance, err := chain.Client.BankQueryClient().Balance(ctx,
-			&banktypes.QueryBalanceRequest{
-				Address: contractAddr,
-				Denom:   nativeDenom,
-			})
-		requireT.NoError(err)
-		requireT.NotNil(contractBalance.Balance)
-		requireT.Equal(nativeDenom, contractBalance.Balance.Denom)
-		requireT.Equal("10000", contractBalance.Balance.Amount.String())
+	testWallet := testing.RandomWallet()
+	withdrawPayload, err := json.Marshal(map[bankMethod]bankWithdrawRequest{
+		withdraw: {
+			Amount:    "5000",
+			Denom:     nativeDenom,
+			Recipient: testWallet.Address().String(),
+		},
+	})
 
-		testWallet := testing.RandomWallet()
-		withdrawPayload, err := json.Marshal(map[bankMethod]bankWithdrawRequest{
-			withdraw: {
-				Amount:    "5000",
-				Denom:     nativeDenom,
-				Recipient: testWallet.Address().String(),
-			},
+	err = wasmTestClient.execute(ctx, contractAddr, withdrawPayload, types.Coin{})
+	requireT.NoError(err)
+
+	// check that contract now has half of the coins
+	contractBalance, err = chain.Client.BankQueryClient().Balance(ctx,
+		&banktypes.QueryBalanceRequest{
+			Address: contractAddr,
+			Denom:   nativeDenom,
 		})
+	requireT.NoError(err)
+	requireT.NotNil(contractBalance.Balance)
+	requireT.Equal(nativeDenom, contractBalance.Balance.Denom)
+	requireT.Equal("5000", contractBalance.Balance.Amount.String())
 
-		err = wasmTestClient.execute(ctx, contractAddr, withdrawPayload, types.Coin{})
-		requireT.NoError(err)
+	// check that the target test wallet has another half
+	testWalletBalance, err := chain.Client.BankQueryClient().Balance(ctx,
+		&banktypes.QueryBalanceRequest{
+			Address: testWallet.Address().String(),
+			Denom:   nativeDenom,
+		})
+	requireT.NoError(err)
+	requireT.NotNil(testWalletBalance.Balance)
+	requireT.Equal(nativeDenom, testWalletBalance.Balance.Denom)
+	requireT.Equal("5000", testWalletBalance.Balance.Amount.String())
+	// bank send invoked by the contract code succeeded! 〠
 
-		// check that contract now has half of the coins
-		contractBalance, err = chain.Client.BankQueryClient().Balance(ctx,
-			&banktypes.QueryBalanceRequest{
-				Address: contractAddr,
-				Denom:   nativeDenom,
-			})
-		requireT.NoError(err)
-		requireT.NotNil(contractBalance.Balance)
-		requireT.Equal(nativeDenom, contractBalance.Balance.Denom)
-		requireT.Equal("5000", contractBalance.Balance.Amount.String())
-
-		// check that the target test wallet has another half
-		testWalletBalance, err := chain.Client.BankQueryClient().Balance(ctx,
-			&banktypes.QueryBalanceRequest{
-				Address: testWallet.Address().String(),
-				Denom:   nativeDenom,
-			})
-		requireT.NoError(err)
-		requireT.NotNil(testWalletBalance.Balance)
-		requireT.Equal(nativeDenom, testWalletBalance.Balance.Denom)
-		requireT.Equal("5000", testWalletBalance.Balance.Amount.String())
-		// bank send invoked by the contract code succeeded! 〠
-	}
-
-	return initTestState, runTestFunc
 }
--- integration-tests/wasm/simple_state.go
+++ integration-tests/wasm/simple_state.go
@@ -5,10 +5,9 @@ import (
 	_ "embed"
 	"encoding/json"
 	"fmt"
-	"math/big"
-
 	"github.com/CoreumFoundation/coreum/pkg/tx"
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 
 	"github.com/CoreumFoundation/coreum/integration-tests/testing"
@@ -33,66 +32,61 @@ const (
 
 // TestSimpleStateWasmContract runs a contract deployment flow and tries to modify the state after deployment.
 // This is a E2E check for the WASM integration, to ensure it works for a simple state contract (Counter).
-func TestSimpleStateWasmContract(chain testing.Chain) (testing.PrepareFunc, testing.RunFunc) {
-	adminWallet := testing.RandomWallet()
-	nativeDenom := chain.Network.TokenSymbol()
+func TestSimpleStateWasmContract(ctx context.Context, t testing.T, chain testing.Chain) {
+	wallet := testing.RandomWallet()
+	nativeDenom := chain.NetworkConfig.TokenSymbol
 
-	initTestState := func(ctx context.Context) error {
-		// FIXME (wojtek): Temporary code for transition
-		if chain.Fund != nil {
-			chain.Fund(adminWallet, types.NewCoinUnsafe(big.NewInt(5000000000), chain.Network.TokenSymbol()))
-		}
-		return nil
-	}
+	requireT := require.New(t)
+	requireT.NoError(chain.Faucet.FundAccounts(ctx,
+		testing.FundedAccount{
+			Wallet: wallet,
+			Amount: testing.MustNewCoin(t, sdk.NewInt(5000000000), nativeDenom),
+		},
+	))
 
-	runTestFunc := func(ctx context.Context, t testing.T) {
-		requireT := require.New(t)
-		wasmTestClient := newWasmTestClient(tx.BaseInput{
-			Signer:   adminWallet,
-			GasPrice: types.NewCoinUnsafe(chain.Network.FeeModel().InitialGasPrice.BigInt(), nativeDenom),
-		}, chain.Client)
+	wasmTestClient := newWasmTestClient(tx.BaseInput{
+		Signer:   wallet,
+		GasPrice: testing.MustNewCoin(t, chain.NetworkConfig.Fee.FeeModel.InitialGasPrice, nativeDenom),
+	}, chain.Client)
 
-		// instantiate the contract and set the initial counter state.
-		// This step could be done within previous step, but separated there, so we could check
-		// the intermediate result of code storage.
-		initialPayload, err := json.Marshal(simpleState{
-			Count: 1337,
-		})
-		requireT.NoError(err)
-		contractAddr, err := wasmTestClient.deployAndInstantiate(
-			ctx,
-			simpleStateWASM,
-			instantiateConfig{
-				accessType: wasmtypes.AccessTypeUnspecified,
-				payload:    initialPayload,
-				label:      "simple_state",
-			},
-		)
-		requireT.NoError(err)
+	// instantiate the contract and set the initial counter state.
+	// This step could be done within previous step, but separated there, so we could check
+	// the intermediate result of code storage.
+	initialPayload, err := json.Marshal(simpleState{
+		Count: 1337,
+	})
+	requireT.NoError(err)
+	contractAddr, err := wasmTestClient.deployAndInstantiate(
+		ctx,
+		simpleStateWASM,
+		instantiateConfig{
+			accessType: wasmtypes.AccessTypeUnspecified,
+			payload:    initialPayload,
+			label:      "simple_state",
+		},
+	)
+	requireT.NoError(err)
 
-		// Query the contract state to get the initial count
-		queryOut, err := wasmTestClient.query(ctx, contractAddr, methodToEmptyBodyPayload(getCount))
-		requireT.NoError(err)
+	// Query the contract state to get the initial count
+	queryOut, err := wasmTestClient.query(ctx, contractAddr, methodToEmptyBodyPayload(getCount))
+	requireT.NoError(err)
 
-		var response simpleState
-		err = json.Unmarshal(queryOut, &response)
-		requireT.NoError(err)
-		requireT.Equal(1337, response.Count)
+	var response simpleState
+	err = json.Unmarshal(queryOut, &response)
+	requireT.NoError(err)
+	requireT.Equal(1337, response.Count)
 
-		// execute contract to increment the count
-		err = wasmTestClient.execute(ctx, contractAddr, methodToEmptyBodyPayload(increment), types.Coin{})
-		requireT.NoError(err)
+	// execute contract to increment the count
+	err = wasmTestClient.execute(ctx, contractAddr, methodToEmptyBodyPayload(increment), types.Coin{})
+	requireT.NoError(err)
 
-		// Query the contract once again to ensure the count has been incremented
-		queryOut, err = wasmTestClient.query(ctx, contractAddr, methodToEmptyBodyPayload(getCount))
-		requireT.NoError(err)
+	// Query the contract once again to ensure the count has been incremented
+	queryOut, err = wasmTestClient.query(ctx, contractAddr, methodToEmptyBodyPayload(getCount))
+	requireT.NoError(err)
 
-		err = json.Unmarshal(queryOut, &response)
-		requireT.NoError(err)
-		requireT.Equal(1338, response.Count)
-	}
-
-	return initTestState, runTestFunc
+	err = json.Unmarshal(queryOut, &response)
+	requireT.NoError(err)
+	requireT.Equal(1338, response.Count)
 }
 
 func methodToEmptyBodyPayload(methodName simpleStateMethod) json.RawMessage {
