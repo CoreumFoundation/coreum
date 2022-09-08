4a95838dbc2c5e755efaebe0af823e41c094b397
Roman Behma <begmaroman@gmail.com>
2022-09-08 18:04:39 +0400

    Minor update

--- integration-tests/testing/network.go
+++ integration-tests/testing/network.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	// MinDepositAmount is the minimum proposal deposit amount to be collected in order to activate it.
-	MinDepositAmount = 10000000
+	MinDepositAmount = 1000
 
 	// MinDepositPeriod is the proposal deposit period duration. Deposit should be made together with the proposal
 	// so not needed to spend more time to make extra deposits.
