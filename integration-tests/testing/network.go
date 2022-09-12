06994f1286d8c9bd6d3fdb5fe8d1c9c03e4066af
Roman Behma <begmaroman@gmail.com>
2022-09-12 09:34:45 +0100

    Increased voting period for test env

--- integration-tests/testing/network.go
+++ integration-tests/testing/network.go
@@ -14,7 +14,7 @@ const (
 	minDepositPeriod = time.Millisecond * 500
 
 	// minVotingPeriod is the proposal voting period duration
-	minVotingPeriod = time.Second * 5
+	minVotingPeriod = time.Second * 10
 )
 
 // NetworkConfig is the network config used by integration tests
