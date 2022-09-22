69d041c52036378aa52b6bc22871c680f7445f4f
Wojtek <103407812+wojtek-coreum@users.noreply.github.com>
2022-09-22 10:57:11 +0200

    Set fractional gas price (#188)
    * Set fractional gas price
    
    * typo
    
    * Merge master into wojtek/fractional-gas-price
    
    * fix
    
    * Merge remote-tracking branch 'origin/wojtek/fractional-gas-price' into wojtek/fractional-gas-price
    
    * Merge master into wojtek/fractional-gas-price
    
    * update doc
    
    * Merge master into wojtek/fractional-gas-price
    
    * Merge master into wojtek/fractional-gas-price
    
    * update charts

--- integration-tests/testing/helpers.go
+++ integration-tests/testing/helpers.go
@@ -17,7 +17,9 @@ func RandomWallet() types.Wallet {
 // ComputeNeededBalance computes the required balance for sending `numOfMessages` number of messages plus some extra amount.
 // FIXME (wojtek): hardcode reasonable default values: https://reviewable.io/reviews/CoreumFoundation/coreum/131#-NA4cljcBl9TBFEqA81t
 func ComputeNeededBalance(gasPrice sdk.Dec, messageGasLimit uint64, numOfMessages int, extraAmount sdk.Int) sdk.Int {
-	return gasPrice.Mul(sdk.NewIntFromUint64(messageGasLimit).ToDec()).TruncateInt().MulRaw(int64(numOfMessages)).Add(extraAmount)
+	// Ceil().RoundInt() is here to be compatible with the sdk's TxFactory
+	// https://github.com/cosmos/cosmos-sdk/blob/ff416ee63d32da5d520a8b2d16b00da762416146/client/tx/factory.go#L223
+	return gasPrice.Mul(sdk.NewIntFromUint64(messageGasLimit).ToDec()).Ceil().RoundInt().MulRaw(int64(numOfMessages)).Add(extraAmount)
 }
 
 // MustNewIntFromString returns a new instance of sdk.Int type from string and fails the test in case of error.
