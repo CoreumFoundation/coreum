08d179d25dd8d95fb19e051053981f336ea0c720
Dzmitry Hil <dzmitryhil@gmail.com>
2022-11-11 10:40:35 +0300

    Integrate the distribution module. (#248)
    * Integrate the distribution module.
    * Deterministic gas price for all distribution module transactions.
    * Integration tests for all distribution module transactions and spend community pool proposal.

---
+++ integration-tests/testing/staking.go
@@ -0,0 +1,84 @@
+package testing
+
+import (
+	"context"
+	"fmt"
+
+	cosmosed25519 "github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+
+	"github.com/CoreumFoundation/coreum-tools/pkg/logger"
+	"github.com/CoreumFoundation/coreum/pkg/tx"
+)
+
+// CreateValidator creates a new validator on the chain and returns the staker addresses, validator addresses and callback function to deactivate it.
+func CreateValidator(ctx context.Context, chain Chain, initialAmount sdk.Int) (sdk.AccAddress, sdk.ValAddress, func() error, error) {
+	stakingClient := stakingtypes.NewQueryClient(chain.ClientContext)
+	staker := chain.GenAccount()
+
+	if err := chain.Faucet.FundAccountsWithOptions(ctx, staker, BalancesOptions{
+		Messages: []sdk.Msg{&stakingtypes.MsgCreateValidator{}, &stakingtypes.MsgUndelegate{}},
+		Amount:   initialAmount,
+	}); err != nil {
+		return nil, nil, nil, err
+	}
+
+	// Create staker
+	validatorAddr := sdk.ValAddress(staker)
+	msg, err := stakingtypes.NewMsgCreateValidator(
+		validatorAddr,
+		cosmosed25519.GenPrivKey().PubKey(),
+		chain.NewCoin(initialAmount),
+		stakingtypes.Description{Moniker: fmt.Sprintf("testing-staker-%s", staker)},
+		stakingtypes.NewCommissionRates(sdk.MustNewDecFromStr("0.1"), sdk.MustNewDecFromStr("0.1"), sdk.MustNewDecFromStr("0.1")),
+		sdk.OneInt(),
+	)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	result, err := tx.BroadcastTx(
+		ctx,
+		chain.ClientContext.WithFromAddress(staker),
+		chain.TxFactory().WithGas(chain.GasLimitByMsgs(msg)),
+		msg,
+	)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	logger.Get(ctx).Info("Validator creation executed", zap.String("txHash", result.TxHash))
+
+	// Make sure staker has been created
+	resp, err := stakingClient.Validator(ctx, &stakingtypes.QueryValidatorRequest{
+		ValidatorAddr: validatorAddr.String(),
+	})
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	if initialAmount.String() != resp.Validator.Tokens.String() {
+		return nil, nil, nil, errors.Errorf("unexpected validator %q tokens after creation: %s", validatorAddr, resp.Validator.Tokens)
+	}
+	if stakingtypes.Bonded != resp.Validator.Status {
+		return nil, nil, nil, errors.Errorf("unexpected validator %q status after creation: %s", validatorAddr, resp.Validator.Status)
+	}
+
+	return staker, validatorAddr, func() error {
+		// Undelegate coins, i.e. deactivate staker
+		undelegateMsg := stakingtypes.NewMsgUndelegate(staker, validatorAddr, chain.NewCoin(initialAmount))
+		_, err = tx.BroadcastTx(
+			ctx,
+			chain.ClientContext.WithFromAddress(staker),
+			chain.TxFactory().WithGas(chain.GasLimitByMsgs(undelegateMsg)),
+			undelegateMsg,
+		)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+
+		return nil
+	}, nil
+}
