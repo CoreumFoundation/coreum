6cb0b4e932b29c4bb7e60286e1e760d4a2bbc647
Dzmitry Hil <dzmitryhil@gmail.com>
2022-11-17 14:27:31 +0300

    Add the FungibleTokenDefinition genesis import and export (#262)

--- x/asset/genesis.go
+++ x/asset/genesis.go
@@ -10,8 +10,10 @@ import (
 
 // InitGenesis initializes the asset module's state from a provided genesis state.
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
-	// TODO(dhil) replace with real implementation
-	// Init Fungible Tokens
+	// Init fungible token definitions
+	for _, definition := range genState.FungibleTokenDefinitions {
+		k.SetFungibleTokenDefinition(ctx, definition)
+	}
 
 	// Init frozen balances
 	for _, frozenBalance := range genState.FrozenBalances {
@@ -22,8 +24,11 @@ func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) 
 
 // ExportGenesis returns the asset module's exported genesis.
 func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
-	// TODO(dhil) replace with real implementation
-	// Export FungibleTokens
+	// Export fungible token definitions
+	fungibleTokenDefinitions, _, err := k.GetFungibleTokenDefinitions(ctx, &query.PageRequest{Limit: query.MaxLimit})
+	if err != nil {
+		panic(err)
+	}
 
 	// Export frozen balances
 	balances, _, err := k.GetAccountsFrozenBalances(ctx, &query.PageRequest{Limit: query.MaxLimit})
@@ -32,6 +37,7 @@ func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
 	}
 
 	return &types.GenesisState{
-		FrozenBalances: balances,
+		FungibleTokenDefinitions: fungibleTokenDefinitions,
+		FrozenBalances:           balances,
 	}
 }
