2683770dbe287fb0416801602477a2802a7533b8
Wojtek <wojtek@coreum.com>
2022-12-06 09:48:29 +0100

    fix

--- x/asset/genesis_test.go
+++ x/asset/genesis_test.go
@@ -16,7 +16,6 @@ import (
 	"github.com/CoreumFoundation/coreum/x/asset/types"
 )
 
-//nolint:funlen
 func TestImportAndExportGenesis(t *testing.T) {
 	assertT := assert.New(t)
 	requireT := require.New(t)
