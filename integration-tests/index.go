fa3f075b5b0f4455691c61bbadd8ad309be2ca98
Milad Zahedi <mza1989@gmail.com>
2022-12-15 16:48:01 +0330

    reverted commented out code

--- integration-tests/index.go
+++ integration-tests/index.go
@@ -2,17 +2,25 @@ package tests
 
 import (
 	"github.com/CoreumFoundation/coreum/integration-tests/asset"
+	"github.com/CoreumFoundation/coreum/integration-tests/auth"
+	"github.com/CoreumFoundation/coreum/integration-tests/bank"
+	"github.com/CoreumFoundation/coreum/integration-tests/distribution"
+	"github.com/CoreumFoundation/coreum/integration-tests/feemodel"
+	"github.com/CoreumFoundation/coreum/integration-tests/gov"
+	"github.com/CoreumFoundation/coreum/integration-tests/staking"
 	"github.com/CoreumFoundation/coreum/integration-tests/testing"
+	"github.com/CoreumFoundation/coreum/integration-tests/upgrade"
+	"github.com/CoreumFoundation/coreum/integration-tests/wasm"
 )
 
 // Tests returns testing environment and tests
 func Tests() []testing.TestSet {
 	return []testing.TestSet{
 		{
-			Name:        "Upgrade",
-			Parallel:    false,
+			Name:     "Upgrade",
+			Parallel: false,
 			SingleChain: []testing.SingleChainSignature{
-				// upgrade.TestUpgrade,
+				upgrade.TestUpgrade,
 			},
 		},
 		{
@@ -27,33 +35,33 @@ func Tests() []testing.TestSet {
 				asset.TestWhitelistFungibleToken,
 				asset.TestMintFungibleToken,
 				asset.TestBurnFungibleToken,
-				// auth.TestUnexpectedSequenceNumber,
-				// auth.TestFeeLimits,
-				// auth.TestMultisig,
-				// bank.TestCoreSend,
-				// bank.TestSendFailsIfNotEnoughGasIsProvided,
-				// bank.TestSendDeterministicGas,
-				// bank.TestSendDeterministicGasTwoBankSends,
-				// bank.TestSendDeterministicGasManyCoins,
-				// bank.TestSendGasEstimation,
-				// bank.TestMultiSendDeterministicGasManyCoins,
-				// bank.TestMultiSend,
-				// bank.TestMultiSendFromMultipleAccounts,
-				// bank.TestMultiSendBatchOutputs,
-				// bank.TestSendBatchMsgs,
-				// distribution.TestWithdrawRewardWithDeterministicGas,
-				// distribution.TestSpendCommunityPoolProposal,
-				// feemodel.TestQueryingMinGasPrice,
-				// feemodel.TestFeeModelProposalParamChange,
-				// staking.TestStakingProposalParamChange,
-				// staking.TestValidatorCRUDAndStaking,
-				// staking.TestValidatorCreationWithLowMinSelfDelegation,
-				// staking.TestValidatorUpdateWithLowMinSelfDelegation,
-				// wasm.TestPinningAndUnpinningSmartContractUsingGovernance,
-				// wasm.TestBankSendWASMContract,
-				// wasm.TestGasWASMBankSendAndBankSend,
-				// gov.TestProposalWithDepositAndWeightedVotes,
-				// wasm.TestIssueFungibleTokenInWASMContract,
+				auth.TestUnexpectedSequenceNumber,
+				auth.TestFeeLimits,
+				auth.TestMultisig,
+				bank.TestCoreSend,
+				bank.TestSendFailsIfNotEnoughGasIsProvided,
+				bank.TestSendDeterministicGas,
+				bank.TestSendDeterministicGasTwoBankSends,
+				bank.TestSendDeterministicGasManyCoins,
+				bank.TestSendGasEstimation,
+				bank.TestMultiSendDeterministicGasManyCoins,
+				bank.TestMultiSend,
+				bank.TestMultiSendFromMultipleAccounts,
+				bank.TestMultiSendBatchOutputs,
+				bank.TestSendBatchMsgs,
+				distribution.TestWithdrawRewardWithDeterministicGas,
+				distribution.TestSpendCommunityPoolProposal,
+				feemodel.TestQueryingMinGasPrice,
+				feemodel.TestFeeModelProposalParamChange,
+				staking.TestStakingProposalParamChange,
+				staking.TestValidatorCRUDAndStaking,
+				staking.TestValidatorCreationWithLowMinSelfDelegation,
+				staking.TestValidatorUpdateWithLowMinSelfDelegation,
+				wasm.TestPinningAndUnpinningSmartContractUsingGovernance,
+				wasm.TestBankSendWASMContract,
+				wasm.TestGasWASMBankSendAndBankSend,
+				gov.TestProposalWithDepositAndWeightedVotes,
+				wasm.TestIssueFungibleTokenInWASMContract,
 			},
 		},
 	}
