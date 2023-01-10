d5b8dac79f1b00ac34064febba6cbdc7ff19fe78
Dzmitry Hil <dzmitryhil@gmail.com>
2023-01-10 16:05:48 +0300

    Fix deterministic_gas_test

--- pkg/config/deterministic_gas_test.go
+++ pkg/config/deterministic_gas_test.go
@@ -79,7 +79,7 @@ func TestDeterministicGasRequirements_DetermMessages(t *testing.T) {
 	// To make sure we do not increase/decrease deterministic types accidentally
 	// we assert length to be equal to exact number, so each change requires
 	// explicit adjustment of tests.
-	assert.Equal(t, 32, len(determMsgs))
+	assert.Equal(t, 33, len(determMsgs))
 	assert.Equal(t, 9, len(undetermMsgs))
 
 	for _, sdkMsg := range determMsgs {
