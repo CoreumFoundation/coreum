8358bd5f34b2d392cb4e7103eace8e31bcf60ca3
Milad Zahedi <mza1989@gmail.com>
2023-01-12 19:27:58 +0330

    pass linter

--- x/asset/ft/keeper/multisend.go
+++ x/asset/ft/keeper/multisend.go
@@ -84,7 +84,7 @@ func (k Keeper) applyRates(ctx sdk.Context, inputs, outputs groupedByDenomAccoun
 }
 
 // CalculateRateShares calculates how the burn or commission share amount should be split between different parties
-func CalculateRateShares(rate sdk.Dec, issuer string, inOps, outOps accountBalanceMap) accountBalanceMap {
+func CalculateRateShares(rate sdk.Dec, issuer string, inOps, outOps accountBalanceMap) map[string]sdk.Int {
 	// The algorithm is as following. we first get the minimum of total inputs and outputs which are not
 	// from the issuer. We then multiply by the rate to get applicable total amount. we then split this amount
 	// between non-issuer senders, proportional to their input value.
