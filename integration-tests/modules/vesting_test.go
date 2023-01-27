0af2781e8b8a3f7cd0ae5e8b5ab715965b35a63a
Dzmitry Hil <dzmitryhil@gmail.com>
2023-01-27 11:47:51 +0300

    Improve the vesting test.

--- integration-tests/modules/vesting_test.go
+++ integration-tests/modules/vesting_test.go
@@ -8,7 +8,6 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-	"github.com/cosmos/cosmos-sdk/types/query"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	vestingtypes "github.com/cosmos/cosmos-sdk/x/auth/vesting/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
@@ -18,6 +17,7 @@ import (
 	integrationtests "github.com/CoreumFoundation/coreum/integration-tests"
 	"github.com/CoreumFoundation/coreum/pkg/tx"
 	assetfttypes "github.com/CoreumFoundation/coreum/x/asset/ft/types"
+	customparamstypes "github.com/CoreumFoundation/coreum/x/customparams/types"
 )
 
 // TestVestingAccountCreationAndBankSend tests vesting account can be created, and it's send limits are applied.
@@ -96,7 +96,11 @@ func TestVestingAccountCreationAndBankSend(t *testing.T) {
 	requireT.True(sdkerrors.ErrInsufficientFunds.Is(err))
 
 	// await vesting time to unlock the vesting coins
-	<-time.After(vestingDuration)
+	select {
+	case <-ctx.Done():
+		return
+	case <-time.After(vestingDuration):
+	}
 
 	// fund the vesting account to pay fees one more time
 	requireT.NoError(chain.Faucet.FundAccountsWithOptions(ctx, vestingAcc, integrationtests.BalancesOptions{
@@ -125,7 +129,7 @@ func TestVestingAccountStaking(t *testing.T) {
 	requireT := require.New(t)
 	authClient := authtypes.NewQueryClient(chain.ClientContext)
 	bankClient := banktypes.NewQueryClient(chain.ClientContext)
-	stakingClient := stakingtypes.NewQueryClient(chain.ClientContext)
+	customParamsClient := customparamstypes.NewQueryClient(chain.ClientContext)
 
 	amountToVest := sdk.NewInt(100)
 	requireT.NoError(chain.Faucet.FundAccountsWithOptions(ctx, creator, integrationtests.BalancesOptions{
@@ -133,7 +137,18 @@ func TestVestingAccountStaking(t *testing.T) {
 		Amount:   amountToVest,
 	}))
 
-	vestingDuration := 10 * time.Second
+	// create new validator
+	customStakingParams, err := customParamsClient.StakingParams(ctx, &customparamstypes.QueryStakingParamsRequest{})
+	require.NoError(t, err)
+	validatorStakingAmount := customStakingParams.Params.MinSelfDelegation
+	_, validatorAddress, deactivateValidator, err := integrationtests.CreateValidator(ctx, chain, validatorStakingAmount, validatorStakingAmount)
+	require.NoError(t, err)
+	defer func() {
+		err := deactivateValidator()
+		require.NoError(t, err)
+	}()
+
+	vestingDuration := time.Hour
 	vestingCoin := chain.NewCoin(amountToVest)
 	createAccMsg := &vestingtypes.MsgCreateVestingAccount{
 		FromAddress: creator.String(),
@@ -174,19 +189,9 @@ func TestVestingAccountStaking(t *testing.T) {
 		},
 	}))
 
-	validatorsRes, err := stakingClient.Validators(ctx, &stakingtypes.QueryValidatorsRequest{
-		Status: stakingtypes.BondStatusBonded,
-		Pagination: &query.PageRequest{
-			Limit: 1,
-		},
-	})
-	requireT.NoError(err)
-
-	validatorOperatorAddress := validatorsRes.Validators[0].OperatorAddress
-
 	msgDelegate := &stakingtypes.MsgDelegate{
 		DelegatorAddress: vestingAcc.String(),
-		ValidatorAddress: validatorOperatorAddress,
+		ValidatorAddress: validatorAddress.String(),
 		Amount:           vestingCoin,
 	}
 
@@ -361,7 +366,11 @@ func TestVestingAccountWithFTInteraction(t *testing.T) {
 	requireT.NoError(err)
 
 	// await vesting time to unlock the vesting coins
-	<-time.After(vestingDuration)
+	select {
+	case <-ctx.Done():
+		return
+	case <-time.After(vestingDuration):
+	}
 
 	// try to burn one more time, now the coins are unlocked so can be burnt
 	_, err = tx.BroadcastTx(
@@ -372,7 +381,7 @@ func TestVestingAccountWithFTInteraction(t *testing.T) {
 	)
 	requireT.NoError(err)
 
-	// try to send unlocker coins
+	// try to send unlocked coins
 	_, err = tx.BroadcastTx(
 		ctx,
 		chain.ClientContext.WithFromAddress(vestingAcc),
