8353c38fccbe8788c4aba4d0257c45aa7137d3e5
Milad Zahedi <mza1989@gmail.com>
2023-03-15 20:59:37 +0330

    fixed some comments and typos in nft module

--- x/asset/nft/keeper/before_transfer.go
+++ x/asset/nft/keeper/before_transfer.go
@@ -4,7 +4,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// BeforeTransfer includes logic that will be run before the Tranfer method of the nft module.
+// BeforeTransfer includes logic that will be run before the Transfer method of the nft module.
 func (k Keeper) BeforeTransfer(ctx sdk.Context, classID, nftID string, receiver sdk.AccAddress) error {
 	if err := k.isNFTSendable(ctx, classID, nftID); err != nil {
 		return err
