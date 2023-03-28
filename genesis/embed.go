5bb90814c2c19a70a2938cb8c050114f4723a9e8
Dzmitry Hil <dzmitryhil@gmail.com>
2023-03-28 13:21:34 +0300

    Fix linters.

--- genesis/embed.go
+++ genesis/embed.go
@@ -6,8 +6,10 @@ import (
 
 var (
 	//go:embed coreum-mainnet-1.json
+	// MainnetGenesis is mainnet genesis file bytes.
 	MainnetGenesis []byte
 
 	//go:embed coreum-testnet-1.json
+	// TestnetGenesis is testnet genesis file bytes.
 	TestnetGenesis []byte
 )
