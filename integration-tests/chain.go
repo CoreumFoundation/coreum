947b0db4849991781f908a1b7dc5f2ee6d5c942f
Dzmitry Hil <dzmitryhil@gmail.com>
2023-05-18 09:55:14 +0300

    * Add chainID to the IBC channels await error.

--- integration-tests/chain.go
+++ integration-tests/chain.go
@@ -276,7 +276,7 @@ func (c ChainContext) GetIBCChannelID(ctx context.Context, peerChainID string) (
 			}
 		}
 
-		return retry.Retryable(errors.New("waiting for channel to open"))
+		return retry.Retryable(errors.Errorf("waiting for for the %s channel on the %s to open", peerChainID, c.ChainSettings.ChainID))
 	}); err != nil {
 		return "", err
 	}
