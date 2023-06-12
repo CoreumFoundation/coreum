28149955dfbf1a611eb4009c42afe7f4ef7e4c67
Wojtek <wojtek@coreum.com>
2023-06-12 14:52:57 +0200

    wip

--- integration-tests/upgrade/ibc_test.go
+++ integration-tests/upgrade/ibc_test.go
@@ -1,16 +1,178 @@
 package upgrade
 
 import (
+	"context"
 	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+
+	integrationtests "github.com/CoreumFoundation/coreum/integration-tests"
+	"github.com/CoreumFoundation/coreum/pkg/client"
+	assetfttypes "github.com/CoreumFoundation/coreum/x/asset/ft/types"
 )
 
 type ibcTest struct {
+	issuer     sdk.AccAddress
+	denomV0AAA string
+	denomV0BBB string
 }
 
 func (i *ibcTest) Before(t *testing.T) {
+	requireT := require.New(t)
 
+	ctx, chain := integrationtests.NewCoreumTestingContext(t)
+	i.issuer = chain.GenAccount()
+
+	chain.FundAccountsWithOptions(ctx, t, i.issuer, integrationtests.BalancesOptions{
+		Messages: []sdk.Msg{
+			&assetfttypes.MsgIssue{},
+			&assetfttypes.MsgIssue{},
+			&assetfttypes.MsgIssue{},
+			&assetfttypes.MsgIssue{},
+			&assetfttypes.MsgTokenUpgradeV1{},
+			&assetfttypes.MsgTokenUpgradeV1{},
+			&assetfttypes.MsgTokenUpgradeV1{},
+		},
+		Amount: getIssueFee(ctx, t, chain.ClientContext).Amount.MulRaw(4),
+	})
+
+	issueMsg := &assetfttypes.MsgIssue{
+		Issuer:        i.issuer.String(),
+		Symbol:        "AAA",
+		Subunit:       "uaaa",
+		Precision:     6,
+		Description:   "AAA Description",
+		InitialAmount: sdk.NewInt(1000),
+		Features: []assetfttypes.Feature{
+			assetfttypes.Feature_minting,
+			assetfttypes.Feature_freezing,
+			assetfttypes.Feature_whitelisting,
+			assetfttypes.Feature_burning,
+		},
+	}
+	_, err := client.BroadcastTx(
+		ctx,
+		chain.ClientContext.WithFromAddress(i.issuer),
+		chain.TxFactory().WithGas(chain.GasLimitByMsgs(issueMsg)),
+		issueMsg,
+	)
+	requireT.NoError(err)
+	i.denomV0AAA = assetfttypes.BuildDenom(issueMsg.Subunit, i.issuer)
+
+	issueMsg = &assetfttypes.MsgIssue{
+		Issuer:        i.issuer.String(),
+		Symbol:        "BBB",
+		Subunit:       "ubbb",
+		Precision:     6,
+		Description:   "BBB Description",
+		InitialAmount: sdk.NewInt(1000),
+		Features: []assetfttypes.Feature{
+			assetfttypes.Feature_minting,
+			assetfttypes.Feature_freezing,
+			assetfttypes.Feature_whitelisting,
+			assetfttypes.Feature_burning,
+		},
+	}
+	_, err = client.BroadcastTx(
+		ctx,
+		chain.ClientContext.WithFromAddress(i.issuer),
+		chain.TxFactory().WithGas(chain.GasLimitByMsgs(issueMsg)),
+		issueMsg,
+	)
+	requireT.NoError(err)
+	i.denomV0BBB = assetfttypes.BuildDenom(issueMsg.Subunit, i.issuer)
+
+	// upgrading token before chain upgrade should not work
+	upgradeMsg := &assetfttypes.MsgTokenUpgradeV1{
+		Sender:     i.issuer.String(),
+		Denom:      i.denomV0AAA,
+		IbcEnabled: true,
+	}
+	_, err = client.BroadcastTx(
+		ctx,
+		chain.ClientContext.WithFromAddress(i.issuer),
+		chain.TxFactory().WithGas(chain.GasLimitByMsgs(upgradeMsg)),
+		upgradeMsg,
+	)
+	requireT.Error(err)
 }
 
 func (i *ibcTest) After(t *testing.T) {
+	requireT := require.New(t)
 
+	ctx, chain := integrationtests.NewCoreumTestingContext(t)
+
+	// issuing token without IBC should succeed
+	issueMsg := &assetfttypes.MsgIssue{
+		Issuer:        i.issuer.String(),
+		Symbol:        "CDE",
+		Subunit:       "ucde",
+		Precision:     6,
+		Description:   "CDE Description",
+		InitialAmount: sdk.NewInt(1000),
+	}
+	_, err := client.BroadcastTx(
+		ctx,
+		chain.ClientContext.WithFromAddress(i.issuer),
+		chain.TxFactory().WithGas(chain.GasLimitByMsgs(issueMsg)),
+		issueMsg,
+	)
+	requireT.NoError(err)
+
+	denomCDE := assetfttypes.BuildDenom(issueMsg.Subunit, i.issuer)
+
+	// issuing token with IBC should succeed after the upgrade
+	issueMsg = &assetfttypes.MsgIssue{
+		Issuer:        i.issuer.String(),
+		Symbol:        "XYZ",
+		Subunit:       "uxyz",
+		Precision:     6,
+		Description:   "XYZ Description",
+		InitialAmount: sdk.NewInt(1000),
+		Features:      []assetfttypes.Feature{assetfttypes.Feature_ibc},
+	}
+	_, err = client.BroadcastTx(
+		ctx,
+		chain.ClientContext.WithFromAddress(i.issuer),
+		chain.TxFactory().WithGas(chain.GasLimitByMsgs(issueMsg)),
+		issueMsg,
+	)
+	requireT.NoError(err)
+	denomXYZ := assetfttypes.BuildDenom(issueMsg.Subunit, i.issuer)
+
+	// upgrading v1 tokens should fail
+	upgradeMsg := &assetfttypes.MsgTokenUpgradeV1{
+		Sender:     i.issuer.String(),
+		Denom:      denomXYZ,
+		IbcEnabled: false,
+	}
+	_, err = client.BroadcastTx(
+		ctx,
+		chain.ClientContext.WithFromAddress(i.issuer),
+		chain.TxFactory().WithGas(chain.GasLimitByMsgs(upgradeMsg)),
+		upgradeMsg,
+	)
+	requireT.Error(err)
+
+	upgradeMsg = &assetfttypes.MsgTokenUpgradeV1{
+		Sender:     i.issuer.String(),
+		Denom:      denomCDE,
+		IbcEnabled: true,
+	}
+	_, err = client.BroadcastTx(
+		ctx,
+		chain.ClientContext.WithFromAddress(i.issuer),
+		chain.TxFactory().WithGas(chain.GasLimitByMsgs(upgradeMsg)),
+		upgradeMsg,
+	)
+	requireT.Error(err)
+}
+
+func getIssueFee(ctx context.Context, t *testing.T, clientCtx client.Context) sdk.Coin {
+	queryClient := assetfttypes.NewQueryClient(clientCtx)
+	resp, err := queryClient.Params(ctx, &assetfttypes.QueryParamsRequest{})
+	require.NoError(t, err)
+
+	return resp.Params.IssueFee
 }
