cf382871037d093a4d3356479898780002870a99
Milad Zahedi <mza1989@gmail.com>
2023-06-12 10:23:13 +0330

    addressed PR comments

--- x/asset/nft/keeper/keeper.go
+++ x/asset/nft/keeper/keeper.go
@@ -361,8 +361,8 @@ func (k Keeper) IsBurnt(ctx sdk.Context, classID, nftID string) (bool, error) {
 	return bytes.Equal(ctx.KVStore(k.storeKey).Get(key), asset.StoreTrue), nil
 }
 
-// ListBurnt return the list of burnt NFTs in class.
-func (k Keeper) ListBurnt(ctx sdk.Context, classID string, q *query.PageRequest) (*query.PageResponse, []string, error) {
+// GetBurnt return the list of burnt NFTs in class.
+func (k Keeper) GetBurnt(ctx sdk.Context, classID string, q *query.PageRequest) (*query.PageResponse, []string, error) {
 	key, err := types.CreateClassBurningKey(classID)
 	if err != nil {
 		return nil, nil, err
