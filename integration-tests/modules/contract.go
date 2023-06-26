b73e5a111a8959fe09c22886d2b2725cd05a331a
keyne <kleuyckx@gmail.com>
2023-06-26 16:06:53 +0100

    wasm files

--- integration-tests/modules/contract.go
+++ integration-tests/modules/contract.go
@@ -9,11 +9,11 @@ var (
 	bankSendWASM []byte
 	//go:embed testdata/wasm/simple-state/artifacts/simple_state.wasm
 	simpleStateWASM []byte
-	//nolint
-	//go:embed testdata/wasm/ft/artifacts/ft.wasm
-	FTWASM []byte
 	//go:embed testdata/wasm/nft/artifacts/nft.wasm
 	nftWASM []byte
 	//go:embed testdata/wasm/authz/artifacts/authz.wasm
 	authzWASM []byte
+	//nolint
+	//go:embed testdata/wasm/ft/artifacts/ft.wasm
+	FTWASM []byte
 )
