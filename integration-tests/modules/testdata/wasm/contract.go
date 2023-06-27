ce903457245e580646072d687a13f7d2573aefe9
keyne <kleuyckx@gmail.com>
2023-06-27 11:35:16 +0100

    pattern syntax

--- integration-tests/modules/testdata/wasm/contract.go
+++ integration-tests/modules/testdata/wasm/contract.go
@@ -8,18 +8,18 @@ import (
 
 var (
 	//nolint
-	//go:embed testdata/wasm/bank-send/artifacts/bank_send.wasm
+	//go:embed bank-send/artifacts/bank_send.wasm
 	BankSendWASM []byte
 	//nolint
-	//go:embed testdata/wasm/simple-state/artifacts/simple_state.wasm
+	//go:embed simple-state/artifacts/simple_state.wasm
 	SimpleStateWASM []byte
 	//nolint
-	//go:embed testdata/wasm/ft/artifacts/ft.wasm
+	//go:embed ft/artifacts/ft.wasm
 	FTWASM []byte
 	//nolint
-	//go:embed testdata/wasm/nft/artifacts/nft.wasm
+	//go:embed nft/artifacts/nft.wasm
 	NftWASM []byte
 	//nolint
-	//go:embed testdata/wasm/authz/artifacts/authz.wasm
+	//go:embed authz/artifacts/authz.wasm
 	AuthzWASM []byte
 )
