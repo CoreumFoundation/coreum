c7226731f1f9de207d2985379ee5392f6d47792c
Milad Zahedi <mza1989@gmail.com>
2023-07-04 16:40:52 +0330

    addressed PR comments

--- x/feemodel/keeper/keeper.go
+++ x/feemodel/keeper/keeper.go
@@ -160,6 +160,11 @@ func (k Keeper) EstimateFutureGasPrice(ctx sdk.Context, after uint32) (sdk.DecCo
 		return sdk.DecCoin{}, sdk.DecCoin{}, sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "after blocks must be lower than or equal to %d", shortEMABlockLength)
 	}
 
+	// if no after value is provide shortEMABlockLength is taken as default value
+	if after == 0 {
+		after = shortEMABlockLength
+	}
+
 	params := k.GetParams(ctx)
 	shortEMA := k.GetShortEMAGas(ctx)
 	longEMA := k.GetLongEMAGas(ctx)
