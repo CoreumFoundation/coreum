deb1d2ad0c27cd38c58d3d50c77a4bb3243f9500
Artem S <vertex451@gmail.com>
2023-07-14 16:57:39 +0200

    wrapped missed error

--- x/delay/types/errors.go
+++ x/delay/types/errors.go
@@ -9,4 +9,6 @@ var (
 	ErrInvalidData = sdkerrors.Register(ModuleName, 1, "invalid data")
 	// ErrInvalidInput error is returned if input data are invalid.
 	ErrInvalidInput = sdkerrors.Register(ModuleName, 2, "invalid input")
+	// ErrInvalidState is returned when state of the module is invalid.
+	ErrInvalidState = sdkerrors.Register(ModuleName, 3, "invalid state")
 )
