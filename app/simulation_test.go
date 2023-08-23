2d58d4d1232c49bfceda43e81ab8e8bf6fc91f34
Wojtek <wojtek@coreum.com>
2023-08-23 14:16:58 +0200

    test

--- app/simulation_test.go
+++ app/simulation_test.go
@@ -25,7 +25,6 @@ func init() {
 // Running using starport command:
 // `starport chain simulate -v --numBlocks 200 --blockSize 50`
 // Running as go benchmark test:
-
 // `go test -benchmem -run=^$ -bench ^BenchmarkSimulation ./app -NumBlocks=200 -BlockSize 50`.
 func BenchmarkSimulation(b *testing.B) {
 	clientcli.FlagEnabledValue = true
