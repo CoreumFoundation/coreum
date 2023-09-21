e95d32d02009b9c51cab5ff1b96b09384da06505
Milad Zahedi <mza1989@gmail.com>
2023-09-21 15:52:53 +0330

    Refactor MultiSend interceptor to work only with 1 input

--- x/asset/ft/keeper/before_send_test.go
+++ x/asset/ft/keeper/before_send_test.go
@@ -2,7 +2,6 @@ package keeper_test
 
 import (
 	"bytes"
-	"math/big"
 	"testing"
 
 	sdkmath "cosmossdk.io/math"
@@ -11,7 +10,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 
 	"github.com/CoreumFoundation/coreum/v3/pkg/config"
 	"github.com/CoreumFoundation/coreum/v3/pkg/config/constant"
@@ -38,7 +36,7 @@ func (k wasmKeeperMock) HasContractInfo(ctx sdk.Context, contractAddress sdk.Acc
 	return exists
 }
 
-func TestCalculateRateShares(t *testing.T) {
+func TestApplyRate(t *testing.T) {
 	genAccount := func() string {
 		return sdk.AccAddress(secp256k1.GenPrivKey().PubKey().Address()).String()
 	}
@@ -61,371 +59,181 @@ func TestCalculateRateShares(t *testing.T) {
 	dummyAddress := genAccount()
 	key := sdk.NewKVStoreKey(types.StoreKey)
 	assetFTKeeper := assetftkeeper.NewKeeper(nil, key, nil, nil, wasmKeeper, "")
-	pow10 := func(ex int64) sdkmath.Int {
-		return sdkmath.NewIntFromBigInt(big.NewInt(0).Exp(big.NewInt(10), big.NewInt(ex), nil))
-	}
+
 	testCases := []struct {
 		name         string
 		rate         string
-		senders      map[string]sdkmath.Int
+		sender       string
 		receivers    map[string]sdkmath.Int
 		ibcDirection wibctransfertypes.Direction
-		shares       map[string]sdkmath.Int
+		appliedRate  sdkmath.Int
 	}{
 		{
-			name:    "empty_senders",
-			rate:    "0.5",
-			senders: map[string]sdkmath.Int{},
-			shares:  map[string]sdkmath.Int{},
-		},
-		{
-			name: "two_senders_issuer_receiver",
-			rate: "0.5",
-			senders: map[string]sdkmath.Int{
-				accounts[0]: sdkmath.NewInt(5),
-				accounts[1]: sdkmath.NewInt(5),
-			},
+			name:   "issuer_receiver",
+			rate:   "0.5",
+			sender: accounts[0],
 			receivers: map[string]sdkmath.Int{
 				issuer: sdkmath.NewInt(10),
 			},
-			shares: map[string]sdkmath.Int{},
+			appliedRate: sdkmath.ZeroInt(),
 		},
 		{
-			name: "issuer_sender_two_receivers",
-			rate: "0.5",
-			senders: map[string]sdkmath.Int{
-				issuer: sdkmath.NewInt(10),
-			},
+			name:   "issuer_sender_two_receivers",
+			rate:   "0.5",
+			sender: issuer,
 			receivers: map[string]sdkmath.Int{
 				accounts[5]: sdkmath.NewInt(5),
 				accounts[6]: sdkmath.NewInt(5),
 			},
-			shares: map[string]sdkmath.Int{},
+			appliedRate: sdkmath.ZeroInt(),
 		},
 		{
-			name: "two_senders_one_receiver",
-			rate: "0.1",
-			senders: map[string]sdkmath.Int{
-				accounts[0]: sdkmath.NewInt(400),
-				accounts[1]: sdkmath.NewInt(600),
-			},
+			name:   "one_receiver",
+			rate:   "0.1",
+			sender: accounts[0],
 			receivers: map[string]sdkmath.Int{
 				accounts[10]: sdkmath.NewInt(1000),
 			},
-			shares: map[string]sdkmath.Int{
-				accounts[0]: sdkmath.NewInt(40),
-				accounts[1]: sdkmath.NewInt(60),
-			},
+			appliedRate: sdkmath.NewInt(100),
 		},
 		{
-			name: "two_senders_one_receiver_with_rounding",
-			rate: "0.1",
-			senders: map[string]sdkmath.Int{
-				accounts[0]: sdkmath.NewInt(399),
-				accounts[1]: sdkmath.NewInt(602),
-			},
+			name:   "one_receiver_with_rounding",
+			rate:   "0.1",
+			sender: accounts[0],
 			receivers: map[string]sdkmath.Int{
 				accounts[10]: sdkmath.NewInt(1001),
 			},
-			shares: map[string]sdkmath.Int{
-				accounts[0]: sdkmath.NewInt(40),
-				accounts[1]: sdkmath.NewInt(61),
-			},
+			appliedRate: sdkmath.NewInt(101),
 		},
 		{
-			name: "issuer_sender_and_two_senders_one_receiver",
-			rate: "0.1",
-			senders: map[string]sdkmath.Int{
-				issuer:      sdkmath.NewInt(90),
-				accounts[0]: sdkmath.NewInt(29),
-				accounts[1]: sdkmath.NewInt(32),
-			},
+			name:   "two_receivers_with_rounding",
+			rate:   "0.1",
+			sender: accounts[0],
 			receivers: map[string]sdkmath.Int{
-				genAccount(): sdkmath.NewInt(90 + 29 + 32),
+				accounts[10]: sdkmath.NewInt(501),
+				accounts[9]:  sdkmath.NewInt(501),
 			},
-			shares: map[string]sdkmath.Int{
-				accounts[0]: sdkmath.NewInt(3),
-				accounts[1]: sdkmath.NewInt(4),
-			},
+			appliedRate: sdkmath.NewInt(101),
 		},
 		{
-			name: "two_senders_issuer_receiver_one_receiver",
-			rate: "0.01",
-			senders: map[string]sdkmath.Int{
-				accounts[0]: sdkmath.NewInt(30000),
-				accounts[1]: sdkmath.NewInt(20000),
+			name:   "issuer_in_receivers",
+			rate:   "0.01",
+			sender: accounts[0],
+			receivers: map[string]sdkmath.Int{
+				issuer:       sdkmath.NewInt(30000),
+				genAccount(): sdkmath.NewInt(20000),
+				genAccount(): sdkmath.NewInt(20000),
 			},
+			appliedRate: sdkmath.NewInt(400),
+		},
+		{
+			name:   "two_receiver_with_issuer_rounding",
+			rate:   "0.01001",
+			sender: accounts[0],
 			receivers: map[string]sdkmath.Int{
 				issuer:       sdkmath.NewInt(30000),
 				genAccount(): sdkmath.NewInt(20000),
 			},
-			shares: map[string]sdkmath.Int{
-				accounts[0]: sdkmath.NewInt(120),
-				accounts[1]: sdkmath.NewInt(80),
-			},
+			appliedRate: sdkmath.NewInt(201),
 		},
 		{
-			name: "two_senders_issuer_receiver_one_receiver_rounding",
-			rate: "0.01001",
-			senders: map[string]sdkmath.Int{
-				accounts[0]: sdkmath.NewInt(30000),
-				accounts[1]: sdkmath.NewInt(20000),
-			},
+			name:   "four_receivers_with_issuer",
+			rate:   "0.01",
+			sender: accounts[0],
 			receivers: map[string]sdkmath.Int{
-				issuer:       sdkmath.NewInt(30000),
-				genAccount(): sdkmath.NewInt(20000),
-			},
-			shares: map[string]sdkmath.Int{
-				accounts[0]: sdkmath.NewInt(121),
-				accounts[1]: sdkmath.NewInt(81),
-			},
-		},
-		{
-			name: "two_senders_one_receiver_rounding",
-			rate: "0.1234",
-			senders: map[string]sdkmath.Int{
-				accounts[0]: sdkmath.NewInt(80),
-				accounts[1]: sdkmath.NewInt(17),
-			},
-			receivers: map[string]sdkmath.Int{
-				genAccount(): sdkmath.NewInt(97),
-			},
-			shares: map[string]sdkmath.Int{
-				accounts[0]: sdkmath.NewInt(10),
-				accounts[1]: sdkmath.NewInt(3),
-			},
-		},
-		{
-			name: "three_senders_one_receiver",
-			rate: "0.1",
-			senders: map[string]sdkmath.Int{
-				accounts[0]: sdkmath.NewInt(1),
-				accounts[1]: sdkmath.NewInt(2),
-				accounts[2]: sdkmath.NewInt(9),
-			},
-			receivers: map[string]sdkmath.Int{
-				genAccount(): sdkmath.NewInt(12),
-			},
-			shares: map[string]sdkmath.Int{
-				accounts[0]: sdkmath.NewInt(1),
-				accounts[1]: sdkmath.NewInt(1),
-				accounts[2]: sdkmath.NewInt(1),
-			},
-		},
-		{
-			name: "issuer_sender_three_senders_issuer_receiver_three_receivers",
-			rate: "0.01",
-			senders: map[string]sdkmath.Int{
-				issuer:      sdkmath.NewInt(2100),
-				accounts[0]: sdkmath.NewInt(1100),
-				accounts[1]: sdkmath.NewInt(1700),
-				accounts[2]: sdkmath.NewInt(1900),
-			},
-			receivers: map[string]sdkmath.Int{
-				issuer:       sdkmath.NewInt(2100),
+				issuer:       sdkmath.NewInt(2101),
 				genAccount(): sdkmath.NewInt(300),
 				genAccount(): sdkmath.NewInt(1100),
 				genAccount(): sdkmath.NewInt(3300),
 			},
-			shares: map[string]sdkmath.Int{
-				accounts[0]: sdkmath.NewInt(11),
-				accounts[1]: sdkmath.NewInt(17),
-				accounts[2]: sdkmath.NewInt(19),
-			},
+			appliedRate: sdkmath.NewInt(47),
 		},
 		{
-			name: "three_senders_three_receivers",
-			rate: "0.01",
-			senders: map[string]sdkmath.Int{
-				accounts[0]: sdkmath.NewInt(100).Mul(pow10(24)),
-				accounts[1]: sdkmath.NewInt(300).Mul(pow10(25)),
-				accounts[2]: sdkmath.NewInt(500).Mul(pow10(26)),
-			},
-			receivers: map[string]sdkmath.Int{
-				genAccount(): sdkmath.NewInt(100).Mul(pow10(24)),
-				genAccount(): sdkmath.NewInt(300).Mul(pow10(25)),
-				genAccount(): sdkmath.NewInt(500).Mul(pow10(26)),
-			},
-			shares: map[string]sdkmath.Int{
-				accounts[0]: sdkmath.NewInt(100).Mul(pow10(22)),
-				accounts[1]: sdkmath.NewInt(300).Mul(pow10(23)),
-				accounts[2]: sdkmath.NewInt(500).Mul(pow10(24)),
-			},
-		},
-		{
-			name: "issuer_sender_three_senders_four_receivers",
-			rate: "0.99",
-			senders: map[string]sdkmath.Int{
-				issuer:      sdkmath.NewInt(2100),
-				accounts[0]: sdkmath.NewInt(1100),
-				accounts[1]: sdkmath.NewInt(1700),
-				accounts[2]: sdkmath.NewInt(2728),
-			},
-			receivers: map[string]sdkmath.Int{
-				genAccount(): sdkmath.NewInt(2100),
-				genAccount(): sdkmath.NewInt(1000),
-				genAccount(): sdkmath.NewInt(1800),
-				genAccount(): sdkmath.NewInt(2728),
-			},
-			shares: map[string]sdkmath.Int{
-				accounts[0]: sdkmath.NewInt(1089),
-				accounts[1]: sdkmath.NewInt(1683),
-				accounts[2]: sdkmath.NewInt(2701),
-			},
-		},
-		{
-			name: "one_sender_ibc",
-			rate: "0.5",
-			senders: map[string]sdkmath.Int{
-				accounts[0]: sdkmath.NewInt(10),
-			},
+			name:   "sender_ibc",
+			rate:   "0.5",
+			sender: accounts[0],
 			receivers: map[string]sdkmath.Int{
 				dummyAddress: sdkmath.NewInt(10),
 			},
 			ibcDirection: wibctransfertypes.PurposeOut,
-			shares: map[string]sdkmath.Int{
-				accounts[0]: sdkmath.NewInt(5),
-			},
+			appliedRate:  sdkmath.NewInt(5),
 		},
 		{
-			name: "issuer_sender_ibc",
-			rate: "0.5",
-			senders: map[string]sdkmath.Int{
-				issuer: sdkmath.NewInt(10),
-			},
+			name:   "issuer_sender_ibc",
+			rate:   "0.5",
+			sender: issuer,
 			receivers: map[string]sdkmath.Int{
 				dummyAddress: sdkmath.NewInt(10),
 			},
 			ibcDirection: wibctransfertypes.PurposeOut,
-			shares:       map[string]sdkmath.Int{},
+			appliedRate:  sdkmath.NewInt(0),
 		},
 		{
-			name: "issuer_sender_two_senders_ibc",
-			rate: "0.5",
-			senders: map[string]sdkmath.Int{
-				issuer:      sdkmath.NewInt(10),
-				accounts[0]: sdkmath.NewInt(10),
-				accounts[1]: sdkmath.NewInt(10),
-			},
-			receivers: map[string]sdkmath.Int{
-				dummyAddress: sdkmath.NewInt(20),
-			},
-			ibcDirection: wibctransfertypes.PurposeOut,
-			shares: map[string]sdkmath.Int{
-				accounts[0]: sdkmath.NewInt(5),
-				accounts[1]: sdkmath.NewInt(5),
-			},
-		},
-		{
-			name: "one_receiver_ibc",
-			rate: "0.5",
-			senders: map[string]sdkmath.Int{
-				dummyAddress: sdkmath.NewInt(10),
-			},
+			name:   "one_receiver_ibc",
+			rate:   "0.5",
+			sender: dummyAddress,
 			receivers: map[string]sdkmath.Int{
 				accounts[0]: sdkmath.NewInt(10),
 			},
 			ibcDirection: wibctransfertypes.PurposeIn,
-			shares:       map[string]sdkmath.Int{},
+			appliedRate:  sdkmath.NewInt(0),
 		},
 		{
-			name: "ibc_escrow_sender_issuer_receiver",
-			rate: "0.5",
-			senders: map[string]sdkmath.Int{
-				dummyAddress: sdkmath.NewInt(10),
-			},
+			name:   "ibc_escrow_sender_issuer_receiver",
+			rate:   "0.5",
+			sender: dummyAddress,
 			receivers: map[string]sdkmath.Int{
 				issuer: sdkmath.NewInt(10),
 			},
 			ibcDirection: wibctransfertypes.PurposeIn,
-			shares:       map[string]sdkmath.Int{},
+			appliedRate:  sdkmath.NewInt(0),
 		},
 		{
-			name: "smart_contract_to_recipient",
-			rate: "0.5",
-			senders: map[string]sdkmath.Int{
-				smartContracts[0]: sdkmath.NewInt(10),
-			},
+			name:   "smart_contract_sender",
+			rate:   "0.5",
+			sender: smartContracts[0],
 			receivers: map[string]sdkmath.Int{
 				dummyAddress: sdkmath.NewInt(10),
 			},
-			shares: map[string]sdkmath.Int{},
+			appliedRate: sdkmath.NewInt(0),
 		},
 		{
-			name: "smart_contract_and_issuer_to_recipient",
-			rate: "0.5",
-			senders: map[string]sdkmath.Int{
-				issuer:            sdkmath.NewInt(5),
-				smartContracts[0]: sdkmath.NewInt(5),
-			},
-			receivers: map[string]sdkmath.Int{
-				dummyAddress: sdkmath.NewInt(10),
-			},
-			shares: map[string]sdkmath.Int{},
-		},
-		{
-			name: "smart_contract_and_issuer_to_smart_contract",
-			rate: "0.5",
-			senders: map[string]sdkmath.Int{
-				issuer:            sdkmath.NewInt(5),
-				smartContracts[0]: sdkmath.NewInt(5),
-			},
-			receivers: map[string]sdkmath.Int{
-				smartContracts[1]: sdkmath.NewInt(10),
-			},
-			shares: map[string]sdkmath.Int{},
-		},
-		{
-			name: "issuer_to_smart_contract",
-			rate: "0.5",
-			senders: map[string]sdkmath.Int{
-				issuer: sdkmath.NewInt(10),
-			},
+			name:   "issuer_to_smart_contract",
+			rate:   "0.5",
+			sender: issuer,
 			receivers: map[string]sdkmath.Int{
 				smartContracts[0]: sdkmath.NewInt(10),
 			},
-			shares: map[string]sdkmath.Int{},
+			appliedRate: sdkmath.NewInt(0),
 		},
 		{
-			name: "sender_to_smart_contract",
-			rate: "0.5",
-			senders: map[string]sdkmath.Int{
-				dummyAddress: sdkmath.NewInt(10),
-			},
+			name:   "smart_contract_receiver",
+			rate:   "0.5",
+			sender: dummyAddress,
 			receivers: map[string]sdkmath.Int{
 				smartContracts[0]: sdkmath.NewInt(10),
 			},
-			shares: map[string]sdkmath.Int{
-				dummyAddress: sdkmath.NewInt(5),
-			},
+			appliedRate: sdkmath.NewInt(5),
 		},
 		{
-			name: "sender_to_smart_contract_and_issuer",
-			rate: "0.5",
-			senders: map[string]sdkmath.Int{
-				dummyAddress: sdkmath.NewInt(10),
-			},
+			name:   "sender_to_smart_contract_and_issuer",
+			rate:   "0.5",
+			sender: dummyAddress,
 			receivers: map[string]sdkmath.Int{
 				smartContracts[0]: sdkmath.NewInt(5),
 				issuer:            sdkmath.NewInt(5),
 			},
-			shares: map[string]sdkmath.Int{
-				dummyAddress: sdkmath.NewInt(3),
-			},
+			appliedRate: sdkmath.NewInt(3),
 		},
 		{
-			name: "sender_to_smart_contracts",
-			rate: "0.5",
-			senders: map[string]sdkmath.Int{
-				dummyAddress: sdkmath.NewInt(10),
-			},
+			name:   "sender_to_smart_contracts",
+			rate:   "0.5",
+			sender: dummyAddress,
 			receivers: map[string]sdkmath.Int{
 				smartContracts[0]: sdkmath.NewInt(5),
 				smartContracts[1]: sdkmath.NewInt(5),
 			},
-			shares: map[string]sdkmath.Int{
-				dummyAddress: sdkmath.NewInt(5),
-			},
+			appliedRate: sdkmath.NewInt(5),
 		},
 	}
 
@@ -439,11 +247,13 @@ func TestCalculateRateShares(t *testing.T) {
 				ctx = wibctransfertypes.WithPurpose(ctx, tc.ibcDirection)
 			}
 
-			shares, err := assetFTKeeper.CalculateRateShares(ctx, sdk.MustNewDecFromStr(tc.rate), issuer, tc.senders, tc.receivers)
-			require.NoError(t, err)
-			for account, share := range shares {
-				assertT.EqualValues(tc.shares[account].String(), share.String())
-			}
+			appliedRate := assetFTKeeper.ApplyRate(
+				ctx,
+				sdk.MustNewDecFromStr(tc.rate),
+				sdk.MustAccAddressFromBech32(issuer),
+				sdk.MustAccAddressFromBech32(tc.sender),
+				tc.receivers)
+			assertT.EqualValues(tc.appliedRate.String(), appliedRate.String())
 		})
 	}
 }
