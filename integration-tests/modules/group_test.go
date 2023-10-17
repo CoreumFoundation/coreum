dcf53794bc957103d8503e1a0e5ef9743e3e29d1
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2023-10-17 19:29:11 +0200

    Implement first version of test

--- integration-tests/modules/group_test.go
+++ integration-tests/modules/group_test.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	bank "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/cosmos/cosmos-sdk/x/group"
 	"github.com/samber/lo"
 	"github.com/stretchr/testify/require"
@@ -26,14 +27,22 @@ func TestGroupCreation(t *testing.T) {
 	// Setup group admin account
 	admin := chain.GenAccount()
 	chain.FundAccountWithOptions(ctx, t, admin, integration.BalancesOptions{
-		Messages: []sdk.Msg{&group.MsgCreateGroup{}},
+		Messages: []sdk.Msg{
+			&group.MsgCreateGroup{},
+			&group.MsgCreateGroupPolicy{},
+		},
 	})
 
 	// Setup group member accounts
+	memberFundBalance := chain.ComputeNeededBalanceFromOptions(integration.BalancesOptions{
+		Messages: []sdk.Msg{&group.MsgVote{}, &group.MsgSubmitProposal{}, &bank.MsgSend{}},
+	})
+	accountsToFund := make([]integration.FundedAccount, 0, 3)
 	groupMembers := lo.Times(3, func(i int) group.MemberRequest {
 		address := chain.GenAccount()
-		chain.FundAccountWithOptions(ctx, t, address, integration.BalancesOptions{
-			Messages: []sdk.Msg{&group.MsgVote{}},
+		accountsToFund = append(accountsToFund, integration.FundedAccount{
+			Address: address,
+			Amount:  chain.NewCoin(memberFundBalance),
 		})
 
 		return group.MemberRequest{
@@ -41,6 +50,8 @@ func TestGroupCreation(t *testing.T) {
 			Weight:  "1",
 		}
 	})
+
+	chain.Faucet.FundAccounts(ctx, t, accountsToFund...)
 
 	// Create group
 	createGroupMsg := group.MsgCreateGroup{
@@ -74,10 +85,11 @@ func TestGroupCreation(t *testing.T) {
 			Threshold: "2",
 			Windows: &group.DecisionPolicyWindows{
 				VotingPeriod:       time.Minute,
-				MinExecutionPeriod: time.Second, // TODO: Rewise.
+				MinExecutionPeriod: time.Millisecond, // TODO: Rewise.
 			},
 		},
 	)
+
 	requireT.NoError(err)
 
 	result, err = client.BroadcastTx(
@@ -96,4 +108,84 @@ func TestGroupCreation(t *testing.T) {
 	requireT.Len(groupPolicies.GroupPolicies, 1)
 	groupPolicy := groupPolicies.GroupPolicies[0]
 	t.Logf("created group policy, groupPolicyAddress:%s txHash:%s", groupPolicy.Address, result.TxHash)
+
+	groupSendCoin := chain.NewCoin(sdk.NewInt(100_000))
+	chain.FundAccountWithOptions(ctx, t, sdk.MustAccAddressFromBech32(groupPolicy.Address), integration.BalancesOptions{
+		Messages: []sdk.Msg{},
+		Amount:   groupSendCoin.Amount,
+	})
+
+	// Create proposal
+	groupCoinReceiver := chain.GenAccount()
+	proposer := sdk.MustAccAddressFromBech32(groupMembers[0].Address)
+	createProposalMsg, err := group.NewMsgSubmitProposal(
+		groupPolicy.Address,
+		[]string{proposer.String()},
+		[]sdk.Msg{&bank.MsgSend{
+			FromAddress: groupPolicy.Address,
+			ToAddress:   groupCoinReceiver.String(),
+			Amount:      []sdk.Coin{groupSendCoin},
+		}},
+		"",
+		group.Exec_EXEC_UNSPECIFIED,
+		"Title",
+		"Summary",
+	)
+	requireT.NoError(err)
+
+	result, err = client.BroadcastTx(
+		ctx,
+		chain.ClientContext.WithFromAddress(proposer),
+		chain.TxFactory().WithGas(chain.GasLimitByMsgs(createProposalMsg)),
+		createProposalMsg,
+	)
+	requireT.NoError(err)
+
+	proposals, err := groupClient.ProposalsByGroupPolicy(ctx, &group.QueryProposalsByGroupPolicyRequest{
+		Address: groupPolicy.Address,
+	})
+	requireT.NoError(err)
+	requireT.Len(proposals.Proposals, 1)
+
+	proposal := proposals.Proposals[0]
+	requireT.Equal(group.PROPOSAL_STATUS_SUBMITTED, proposal.Status)
+	t.Logf("submitted group proposal, id:%d txHash:%s", proposal.Id, result.TxHash)
+
+	// Vote for proposal
+	for i := 0; i < 2; i++ {
+		voter := sdk.MustAccAddressFromBech32(groupMembers[i+1].Address)
+		voteMsg := &group.MsgVote{
+			ProposalId: proposal.Id,
+			Voter:      voter.String(),
+			Option:     group.VOTE_OPTION_YES,
+			Exec:       group.Exec_EXEC_TRY,
+		}
+
+		result, err = client.BroadcastTx(
+			ctx,
+			chain.ClientContext.WithFromAddress(voter),
+			chain.TxFactory().WithGas(chain.GasLimitByMsgs(voteMsg)),
+			voteMsg,
+		)
+		requireT.NoError(err)
+	}
+
+	//tally, err := groupClient.TallyResult(ctx, &group.QueryTallyResultRequest{
+	//	ProposalId: proposal.Id,
+	//})
+	//requireT.NoError(err)
+	//tally = tally
+	_, err = groupClient.Proposal(ctx, &group.QueryProposalRequest{
+		ProposalId: proposal.Id,
+	})
+	requireT.Error(err)
+
+	bankClient := bank.NewQueryClient(chain.ClientContext)
+	receiverBalance, err := bankClient.Balance(ctx, &bank.QueryBalanceRequest{
+		Address: groupCoinReceiver.String(),
+		Denom:   chain.ChainSettings.Denom,
+	})
+	requireT.NoError(err)
+
+	requireT.Equal(groupSendCoin.Amount, receiverBalance.Balance.Amount)
 }
