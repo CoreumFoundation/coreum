d3f7312040cb8dc15a25ca72dc5824a2a9a488d4
milad <mza1989@gmail.com>
2023-11-13 16:26:02 +0330

    Fix cli crash on command error

--- cmd/cored/main.go
+++ cmd/cored/main.go
@@ -7,7 +7,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/server"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
-	"github.com/pkg/errors"
 
 	"github.com/CoreumFoundation/coreum/v3/app"
 	"github.com/CoreumFoundation/coreum/v3/cmd/cored/cosmoscmd"
@@ -29,11 +28,12 @@ func main() {
 	rootCmd.PersistentFlags().String(flags.FlagChainID, string(app.DefaultChainID), "The network chain ID")
 	if err := svrcmd.Execute(rootCmd, coreumEnvPrefix, app.DefaultNodeHome); err != nil {
 		fmt.Printf("Error executing cmd, err: %s", err)
-		var errCode server.ErrorCode
-		if errors.As(err, errCode) {
-			os.Exit(errCode.Code)
-		}
+		switch e := err.(type) {
+		case server.ErrorCode:
+			os.Exit(e.Code)
 
-		os.Exit(1)
+		default:
+			os.Exit(1)
+		}
 	}
 }
