334d43120830b29dc9baf4271e1f5c2932353b15
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2024-04-10 13:03:22 +0200

    Integration tests implementation

--- integration-tests/contracts/ibc/ibc-hooks-counter.go
+++ integration-tests/contracts/ibc/ibc-hooks-counter.go
@@ -1,5 +1,24 @@
 package ibc
 
+import sdk "github.com/cosmos/cosmos-sdk/types"
+
+// SimpleStateMethod is a type used to represent the methods available inside the simple state contract.
+type HooksMethod string
+
+const (
+	HooksGetCount HooksMethod = "get_count"
+
+	HooksGetTotalFunds HooksMethod = "get_total_funds"
+)
+
 type HooksCounterState struct {
 	Count int `json:"count"`
+}
+
+type HooksTotalFundsState struct {
+	TotalFunds sdk.Coins `json:"total_funds"`
+}
+
+type HooksBodyRequest struct {
+	Addr string `json:"addr"`
 }
--- integration-tests/ibc/ibchooks_test.go
+++ integration-tests/ibc/ibchooks_test.go
@@ -3,6 +3,7 @@
 package ibc
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"testing"
@@ -10,12 +11,14 @@ import (
 
 	sdkmath "cosmossdk.io/math"
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
-	"github.com/cosmos/cosmos-sdk/client/grpc/tmservice"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdktx "github.com/cosmos/cosmos-sdk/types/tx"
 	ibctransfertypes "github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
 
+	ibchookskeeper "github.com/cosmos/ibc-apps/modules/ibc-hooks/v7/keeper"
+
+	"github.com/CoreumFoundation/coreum-tools/pkg/retry"
 	integrationtests "github.com/CoreumFoundation/coreum/v4/integration-tests"
 	ibcwasm "github.com/CoreumFoundation/coreum/v4/integration-tests/contracts/ibc"
 	"github.com/CoreumFoundation/coreum/v4/testutil/integration"
@@ -40,7 +43,7 @@ func TestIBCHooksCounter(t *testing.T) {
 
 	coreumContractAdmin := coreumChain.GenAccount()
 	coreumCaller := coreumChain.GenAccount()
-	coreumRecipient := coreumChain.GenAccount()
+	//coreumRecipient := coreumChain.GenAccount()
 
 	osmosisContractAdmin := osmosisChain.GenAccount()
 	osmosisCaller := osmosisChain.GenAccount()
@@ -117,7 +120,10 @@ func TestIBCHooksCounter(t *testing.T) {
 	coreumToOsmosisChannelID := coreumChain.AwaitForIBCChannelID(
 		ctx, t, ibctransfertypes.PortID, osmosisChain.ChainSettings.ChainID,
 	)
-	_ = coreumToOsmosisChannelID
+
+	ibcHookCallerOnCoreumAddr, err := ibchookskeeper.DeriveIntermediateSender(coreumToOsmosisChannelID, osmosisChain.MustConvertToBech32Address(osmosisCaller), coreumChain.Chain.ChainSettings.AddressPrefix)
+	requireT.NoError(err)
+	fmt.Println(ibcHookCallerOnCoreumAddr)
 
 	txRes, err = osmosisChain.ExecuteIBCTransferWithMemo(
 		ctx,
@@ -128,56 +134,131 @@ func TestIBCHooksCounter(t *testing.T) {
 		coreumContractAddr, // can be empty string ?
 		fmt.Sprintf(`{"wasm":{"contract": "%s", "msg":{"increment":{}}}}`, coreumContractAddr),
 	)
-	//requireT.NoError(err)
-	fmt.Println(txRes.RawLog)
-	fmt.Println(txRes.TxHash)
+	awaitHooksContractState(
+		ctx,
+		t,
+		coreumChain,
+		coreumContractAddr,
+		ibcHookCallerOnCoreumAddr,
+		0,
+		sdk.Coins{coreumChain.NewCoin(sendToCoreumCoin.Amount)},
+	)
 
-	tmQueryClient := tmservice.NewServiceClient(coreumChain.ClientContext)
-	res, err := tmQueryClient.GetLatestBlock(ctx, &tmservice.GetLatestBlockRequest{})
-	require.NoError(t, err)
+	txRes, err = osmosisChain.ExecuteIBCTransferWithMemo(
+		ctx,
+		t,
+		osmosisCaller,
+		sendToCoreumCoin,
+		coreumChain.ChainContext,
+		coreumContractAddr, // can be empty string ?
+		fmt.Sprintf(`{"wasm":{"contract": "%s", "msg":{"increment":{}}}}`, coreumContractAddr),
+	)
+	awaitHooksContractState(
+		ctx,
+		t,
+		coreumChain,
+		coreumContractAddr,
+		ibcHookCallerOnCoreumAddr,
+		1,
+		sdk.Coins{coreumChain.NewCoin(sendToCoreumCoin.Amount.Add(sendToCoreumCoin.Amount))},
+	)
 
-	txSvcClient := sdktx.NewServiceClient(coreumChain.ClientContext)
+	//tmQueryClient := tmservice.NewServiceClient(coreumChain.ClientContext)
+	//res, err := tmQueryClient.GetLatestBlock(ctx, &tmservice.GetLatestBlockRequest{})
+	//require.NoError(t, err)
 
-	currentHeight := blockHeightFromResponse(res)
-	for block := currentHeight - 10; block < currentHeight+25; block++ {
-		fmt.Printf("querying block: %v\n", block)
-		res, err := txSvcClient.GetBlockWithTxs(ctx, &sdktx.GetBlockWithTxsRequest{Height: block})
-		if err != nil {
-			fmt.Println("block not found waiting for 2s")
-			<-time.After(2 * time.Second)
-			block--
-			continue
-		}
-
-		if len(res.Txs) > 0 {
-			fmt.Printf("total txs in block %v: %v\n", res.Block.Header.Height, len(res.Txs))
-
-			//res.Txs[0].Signatures
-			//fmt.Printf("txid: %v", .Body.)
-		}
-	}
+	//txSvcClient := sdktx.NewServiceClient(coreumChain.ClientContext)
+	//
+	//currentHeight := blockHeightFromResponse(res)
+	//for block := currentHeight - 10; block < currentHeight+25; block++ {
+	//	fmt.Printf("querying block: %v\n", block)
+	//	res, err := txSvcClient.GetBlockWithTxs(ctx, &sdktx.GetBlockWithTxsRequest{Height: block})
+	//	if err != nil {
+	//		fmt.Println("block not found waiting for 2s")
+	//		<-time.After(2 * time.Second)
+	//		block--
+	//		continue
+	//	}
+	//
+	//	if len(res.Txs) > 0 {
+	//		fmt.Printf("total txs in block %v: %v\n", res.Block.Header.Height, len(res.Txs))
+	//
+	//		//res.Txs[0].Signatures
+	//		//fmt.Printf("txid: %v", .Body.)
+	//	}
+	//}
 
 	// ***** Send IBC Transfer back *****///
 
-	if true {
-		txRes, err = osmosisChain.ExecuteIBCTransfer(
-			ctx, t, osmosisCaller, sendToCoreumCoin, coreumChain.ChainContext, coreumRecipient,
-		)
-		requireT.NoError(err)
-		fmt.Println(txRes.TxHash)
-
-		expectedCoreumRecipientBalance := coreumChain.NewCoin(sendToCoreumCoin.Amount)
-		requireT.NoError(coreumChain.AwaitForBalance(ctx, t, coreumRecipient, expectedCoreumRecipientBalance))
-
-		fmt.Println("sleeping before sending IBC hook tx")
-		<-time.After(time.Second * 10)
-	}
+	//if true {
+	//	txRes, err = osmosisChain.ExecuteIBCTransfer(
+	//		ctx, t, osmosisCaller, sendToCoreumCoin, coreumChain.ChainContext, coreumRecipient,
+	//	)
+	//	requireT.NoError(err)
+	//	fmt.Println(txRes.TxHash)
+	//
+	//	expectedCoreumRecipientBalance := coreumChain.NewCoin(sendToCoreumCoin.Amount)
+	//	requireT.NoError(coreumChain.AwaitForBalance(ctx, t, coreumRecipient, expectedCoreumRecipientBalance))
+	//
+	//	fmt.Println("sleeping before sending IBC hook tx")
+	//	<-time.After(time.Second * 10)
+	//}
 }
 
-func blockHeightFromResponse(res *tmservice.GetLatestBlockResponse) int64 {
-	if res.SdkBlock != nil {
-		return res.SdkBlock.Header.Height
-	}
+func awaitHooksContractState(
+	ctx context.Context,
+	t *testing.T,
+	coreumChain integration.CoreumChain,
+	contractAddr string,
+	callerAddr string,
+	expectedCount int,
+	expectedFunds sdk.Coins,
+) {
+	t.Helper()
 
-	return res.Block.Header.Height //nolint:staticcheck // we keep it to keep the compatibility with old versions
+	t.Logf("Awaiting for contract state contract:%s count:%d total_funds:%s",
+		contractAddr, expectedCount, expectedFunds.String())
+
+	retryCtx, retryCancel := context.WithTimeout(ctx, time.Minute)
+	defer retryCancel()
+	require.NoError(t, retry.Do(retryCtx, time.Second, func() error {
+		getCountPayload, err := json.Marshal(map[ibcwasm.HooksMethod]ibcwasm.HooksBodyRequest{
+			ibcwasm.HooksGetCount: {
+				Addr: callerAddr,
+			},
+		})
+		queryCountOut, err := coreumChain.Wasm.QueryWASMContract(ctx, contractAddr, getCountPayload)
+		require.NoError(t, err)
+
+		var countResponse ibcwasm.HooksCounterState
+		require.NoError(t, json.Unmarshal(queryCountOut, &countResponse))
+		if countResponse.Count != expectedCount {
+			return retry.Retryable(errors.Errorf(
+				"counter is still not equal to expected, current:%d, expected:%d",
+				countResponse.Count,
+				expectedCount,
+			))
+		}
+
+		getTotalFundsPayload, err := json.Marshal(map[ibcwasm.HooksMethod]ibcwasm.HooksBodyRequest{
+			ibcwasm.HooksGetTotalFunds: {
+				Addr: callerAddr,
+			},
+		})
+		queryTotalFundsOut, err := coreumChain.Wasm.QueryWASMContract(ctx, contractAddr, getTotalFundsPayload)
+		require.NoError(t, err)
+		var totalFundsResponse ibcwasm.HooksTotalFundsState
+		require.NoError(t, json.Unmarshal(queryTotalFundsOut, &totalFundsResponse))
+		if !totalFundsResponse.TotalFunds.IsEqual(expectedFunds) {
+			return retry.Retryable(errors.Errorf(
+				"total_funds is still not equal to expected, current:%s, expected:%s",
+				totalFundsResponse.TotalFunds.String(),
+				expectedFunds.String(),
+			))
+		}
+		require.Equal(t, expectedFunds.String(), totalFundsResponse.TotalFunds.String())
+
+		return nil
+	}))
+
 }
