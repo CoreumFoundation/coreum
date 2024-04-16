3c5ad4ac368c06e3df375da190204b6ed5691f3d
Wojtek <103407812+wojtek-coreum@users.noreply.github.com>
2024-04-16 09:42:25 +0200

    Migrate CI to use `coreum-builder` (#798)

--- build/cmd/main.go
+++ build/cmd/main.go
@@ -1,36 +1,10 @@
 package main
 
 import (
-	"context"
-	"os"
-	"path/filepath"
-
 	"github.com/CoreumFoundation/coreum-tools/pkg/build"
-	"github.com/CoreumFoundation/coreum-tools/pkg/logger"
-	"github.com/CoreumFoundation/coreum-tools/pkg/must"
-	"github.com/CoreumFoundation/coreum-tools/pkg/run"
 	selfBuild "github.com/CoreumFoundation/coreum/build"
 )
 
 func main() {
-	run.Tool("build", func(ctx context.Context) error {
-		flags := logger.Flags(logger.ToolDefaultConfig, "build")
-		if err := flags.Parse(os.Args[1:]); err != nil {
-			return err
-		}
-		exec := build.NewExecutor(selfBuild.Commands)
-		if build.Autocomplete(exec) {
-			return nil
-		}
-
-		changeWorkingDir()
-		return build.Do(ctx, "coreum", flags.Args(), exec)
-	})
-}
-
-// changeWorkingDir sets working dir to the root directory of repository.
-func changeWorkingDir() {
-	must.OK(os.Chdir(filepath.Dir(filepath.Dir(filepath.Dir(must.String(
-		filepath.EvalSymlinks(must.String(os.Executable())),
-	))))))
+	build.Main(selfBuild.Commands)
 }
