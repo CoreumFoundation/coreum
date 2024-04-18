ef3ce7fdb682562b7de75420e6890aa51c0f61ae
Masih Yeganeh <goodboy.php@gmail.com>
2024-04-18 11:00:41 +0330

    Add admin migration script

--- x/asset/ft/keeper/migrations.go
+++ x/asset/ft/keeper/migrations.go
@@ -5,6 +5,7 @@ import (
 
 	v1 "github.com/CoreumFoundation/coreum/v4/x/asset/ft/migrations/v1"
 	v2 "github.com/CoreumFoundation/coreum/v4/x/asset/ft/migrations/v2"
+	v3 "github.com/CoreumFoundation/coreum/v4/x/asset/ft/migrations/v3"
 )
 
 // Migrator is a struct for handling in-place store migrations.
@@ -32,4 +33,9 @@ func (m Migrator) Migrate1to2(ctx sdk.Context) error {
 // Migrate2to3 migrates from version 2 to 3.
 func (m Migrator) Migrate2to3(ctx sdk.Context) error {
 	return v2.MigrateParams(ctx, m.ftKeeper, m.paramsKeeper)
+}
+
+// Migrate3to4 migrates from version 3 to 4.
+func (m Migrator) Migrate3to4(ctx sdk.Context) error {
+	return v3.MigrateDefinitions(ctx, m.ftKeeper)
 }
