e5e50a545399d3dc9f461ed8bbd9d68a99a0508f
Masih Yeganeh <goodboy.php@gmail.com>
2024-04-29 13:54:40 +0330

    Fix error messages

--- x/asset/ft/keeper/keeper.go
+++ x/asset/ft/keeper/keeper.go
@@ -617,7 +617,7 @@ func (k Keeper) SetWhitelistedBalance(ctx sdk.Context, sender, addr sdk.AccAddre
 	}
 
 	if def.IsAdmin(addr) {
-		return sdkerrors.Wrap(cosmoserrors.ErrUnauthorized, "issuer's balance can't be whitelisted")
+		return sdkerrors.Wrap(cosmoserrors.ErrUnauthorized, "admin's balance can't be whitelisted")
 	}
 
 	if err = def.CheckFeatureAllowed(sender, types.Feature_whitelisting); err != nil {
@@ -1052,7 +1052,7 @@ func (k Keeper) freezingChecks(ctx sdk.Context, sender, addr sdk.AccAddress, coi
 	}
 
 	if def.IsAdmin(addr) {
-		return sdkerrors.Wrap(cosmoserrors.ErrUnauthorized, "issuer's balance can't be frozen")
+		return sdkerrors.Wrap(cosmoserrors.ErrUnauthorized, "admin's balance can't be frozen")
 	}
 
 	return def.CheckFeatureAllowed(sender, types.Feature_freezing)
