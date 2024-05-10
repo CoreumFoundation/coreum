2a3a809a68799d249efa5ce7a19997052faab668
Masih Yeganeh <goodboy.php@gmail.com>
2024-05-10 10:52:27 +0330

    Change ClearAdmin logic

--- x/asset/ft/keeper/keeper.go
+++ x/asset/ft/keeper/keeper.go
@@ -616,7 +616,7 @@ func (k Keeper) SetWhitelistedBalance(ctx sdk.Context, sender, addr sdk.AccAddre
 		return sdkerrors.Wrapf(err, "not able to get token info for denom:%s", coin.Denom)
 	}
 
-	if def.HasAdminPrivileges(addr) {
+	if def.IsAdmin(addr) {
 		return sdkerrors.Wrap(cosmoserrors.ErrUnauthorized, "admin's balance can't be whitelisted")
 	}
 
@@ -731,7 +731,9 @@ func (k Keeper) ClearAdmin(ctx sdk.Context, sender sdk.AccAddress, denom string)
 	}
 
 	def.Admin = ""
-	def.SendCommissionRate = sdk.ZeroDec()
+	if !def.IsFeatureEnabled(types.Feature_extension) {
+		def.SendCommissionRate = sdk.ZeroDec()
+	}
 
 	k.SetDefinition(ctx, issuer, subunit, def)
 
@@ -1070,10 +1072,6 @@ func (k Keeper) validateClawbackAllowed(ctx sdk.Context, sender, addr sdk.AccAdd
 	def, err := k.GetDefinition(ctx, coin.Denom)
 	if err != nil {
 		return sdkerrors.Wrapf(err, "not able to get token info for denom:%s", coin.Denom)
-	}
-
-	if def.HasAdminPrivileges(addr) {
-		return sdkerrors.Wrap(cosmoserrors.ErrUnauthorized, "admin's balance can't be clawed back")
 	}
 
 	if _, isModuleAccount := k.accountKeeper.GetAccount(ctx, addr).(*authtypes.ModuleAccount); isModuleAccount {
