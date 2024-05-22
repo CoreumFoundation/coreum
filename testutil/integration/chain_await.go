040880851acaf48da39eafda3a12cb3ad04d5aa0
Dzmitry Hil <dzmitryhil@gmail.com>
2024-05-22 12:24:17 +0300

    Add crust ref

--- testutil/integration/chain_await.go
+++ testutil/integration/chain_await.go
@@ -9,7 +9,7 @@ import (
 
 const (
 	// AwaitStateTimeout is duration to await for account to have a specific balance.
-	AwaitStateTimeout = time.Minute
+	AwaitStateTimeout = 30 * time.Millisecond
 	// awaitRecheckTimeout is duration between the state recheck.
 	awaitRecheckDelay = 100 * time.Millisecond
 	// awaitCheckTimeout is timeout for a single check.
