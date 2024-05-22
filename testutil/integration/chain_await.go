a2f43812d3c86a006f5076b45d3e7b89e07ca3d9
Dzmitry Hil <dzmitryhil@gmail.com>
2024-05-22 12:20:12 +0300

    Increase IBC await timeout.

--- testutil/integration/chain_await.go
+++ testutil/integration/chain_await.go
@@ -9,7 +9,7 @@ import (
 
 const (
 	// AwaitStateTimeout is duration to await for account to have a specific balance.
-	AwaitStateTimeout = 30 * time.Second
+	AwaitStateTimeout = time.Millisecond
 	// awaitRecheckTimeout is duration between the state recheck.
 	awaitRecheckDelay = 100 * time.Millisecond
 	// awaitCheckTimeout is timeout for a single check.
