3f889dbc2fe2c4405f8444f8ba817426713c1b66
Wojtek <wojtek@coreum.com>
2024-05-24 12:26:45 +0200

    Script downloading cored

---
+++ setup-client.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+VERSION="v3.0.3"
+BIN_PATH="$HOME/bin/cored"
+
+URL_LINUX_AMD64="https://github.com/CoreumFoundation/coreum/releases/download/${VERSION}/cored-linux-amd64"
+URL_LINUX_ARM64="https://github.com/CoreumFoundation/coreum/releases/download/${VERSION}/cored-linux-arm64"
+URL_DARWIN_AMD64="https://github.com/CoreumFoundation/coreum/releases/download/${VERSION}/cored-client-darwin-amd64"
+URL_DARWIN_ARM64="https://github.com/CoreumFoundation/coreum/releases/download/${VERSION}/cored-client-darwin-arm64"
+
+PLATFORM="$(uname -o)/$(uname -m)"
+URL=""
+
+case "$PLATFORM" in
+  "GNU/Linux/x86_64") URL=$URL_LINUX_AMD64 ;;
+  "GNU/Linux/arm64") URL=$URL_LINUX_ARM64 ;;
+  "DARWIN/x86_64") URL=$URL_DARWIN_AMD64 ;;
+  "DARWIN/arm64") URL=$URL_DARWIN_ARM64 ;;
+  *) echo "Unsupported platform $PLATFORM"; exit 1
+esac
+
+echo "Downloading Coreum client..."
+
+curl -L "$URL" --output "$BIN_PATH" --create-dirs
+chmod u+x "$BIN_PATH"
+
+echo "Coreum client installed in ${BIN_PATH}"
