6c4d2a444846f8558aef9b77498417c744b54207
Wojtek <103407812+wojtek-coreum@users.noreply.github.com>
2024-06-03 16:27:14 +0200

    Build local `znet` (#862)
    * Build local `znet`
    
    * linter
    
    * Merge master into wojtek/local-znet
    
    * Merge remote-tracking branch 'origin/master' into wojtek/local-znet
    
    # Conflicts:
    #	build/index.go
    
    * fixes

--- build/index.go
+++ build/index.go
@@ -10,7 +10,8 @@ import (
 
 // Commands is a definition of commands available in build system.
 var Commands = map[string]types.Command{
-	"build/me": {Fn: crust.BuildBuilder, Description: "Builds the builder"},
+	"build/me":   {Fn: crust.BuildBuilder, Description: "Builds the builder"},
+	"build/znet": {Fn: crust.BuildZNet, Description: "Builds znet binary"},
 	"build": {Fn: func(ctx context.Context, deps types.DepsFunc) error {
 		deps(
 			coreum.BuildCored,
