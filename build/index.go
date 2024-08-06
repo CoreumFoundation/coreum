b5524bd926abd28e93abb682ca54a98a67dcaa38
milad <mza1989@gmail.com>
2024-08-06 13:18:02 +0330

    fix build/me

--- build/index.go
+++ build/index.go
@@ -11,7 +11,7 @@ import (
 
 // Commands is a definition of commands available in build system.
 var Commands = map[string]types.Command{
-	"build/me":   {Fn: crust.BuildBuilder, Description: "Builds the builder"},
+	"build/me":   {Fn: coreum.BuildBuilder, Description: "Builds the builder"},
 	"build/znet": {Fn: crust.BuildZNet, Description: "Builds znet binary"},
 	"build": {Fn: func(ctx context.Context, deps types.DepsFunc) error {
 		deps(
