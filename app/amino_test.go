6d713b56b479dcad33fb2237b6fdc45ec9f38beb
Masih Yeganeh <goodboy.php@gmail.com>
2024-09-05 23:33:58 +0330

    Fix TestLegacyAmino_ExpectedMessages

--- app/amino_test.go
+++ app/amino_test.go
@@ -6,24 +6,25 @@ import (
 	"testing"
 	_ "unsafe"
 
+	"cosmossdk.io/api/amino"
+	msgv1 "cosmossdk.io/api/cosmos/msg/v1"
+	"cosmossdk.io/x/feegrant"
 	"cosmossdk.io/x/nft"
-	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 	sdktestdatatypes "github.com/cosmos/cosmos-sdk/testutil/testdata"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdktxtypes "github.com/cosmos/cosmos-sdk/types/tx"
 	"github.com/cosmos/cosmos-sdk/x/auth/migrations/legacytx"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	govtypesv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
-	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
-	slashingtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
-	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
-	packetforwardtypes "github.com/cosmos/ibc-apps/middleware/packet-forward-middleware/v8/packetforward/types"
-	ibcinterchainaccountstypes "github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts/controller/types"
+	ibcinterchainaccountscontrollertypes "github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts/controller/types"
+	ibcinterchainaccountshosttypes "github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts/host/types"
+	ibctransfertypes "github.com/cosmos/ibc-go/v8/modules/apps/transfer/types"
 	ibcclienttypes "github.com/cosmos/ibc-go/v8/modules/core/02-client/types"
 	ibcconnectiontypes "github.com/cosmos/ibc-go/v8/modules/core/03-connection/types"
 	ibcchanneltypes "github.com/cosmos/ibc-go/v8/modules/core/04-channel/types"
+	proto2 "github.com/golang/protobuf/proto"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
 )
 
 // To access private variable from github.com/cosmos/gogoproto we link it to local variable.
@@ -33,33 +34,15 @@ import (
 var revProtoTypes map[reflect.Type]string
 
 func TestLegacyAmino_ExpectedMessages(t *testing.T) {
-	// TODO(fix-tests)
-	t.SkipNow()
 	expectedNonAminoMsgURLs := map[string]struct{}{
-		// auth
-		sdk.MsgTypeURL(&authtypes.MsgUpdateParams{}): {},
-
 		// bank
-		sdk.MsgTypeURL(&banktypes.MsgUpdateParams{}):   {},
-		sdk.MsgTypeURL(&banktypes.MsgSetSendEnabled{}): {},
+		sdk.MsgTypeURL(&banktypes.Input{}): {},
 
 		// nft
 		sdk.MsgTypeURL(&nft.MsgSend{}): {},
 
 		// gov
-		sdk.MsgTypeURL(&govtypesv1.MsgExecLegacyContent{}): {},
-
-		// mint
-		sdk.MsgTypeURL(&minttypes.MsgUpdateParams{}): {},
-
-		// pfm
-		sdk.MsgTypeURL(&packetforwardtypes.MsgUpdateParams{}): {},
-
-		// slashing
-		sdk.MsgTypeURL(&slashingtypes.MsgUpdateParams{}): {},
-
-		// staking
-		sdk.MsgTypeURL(&stakingtypes.MsgUpdateParams{}): {},
+		sdk.MsgTypeURL(&govtypesv1.MsgCancelProposal{}): {},
 
 		// ibc/core/client
 		sdk.MsgTypeURL(&ibcclienttypes.MsgCreateClient{}):  {},
@@ -67,47 +50,81 @@ func TestLegacyAmino_ExpectedMessages(t *testing.T) {
 		sdk.MsgTypeURL(&ibcclienttypes.MsgUpgradeClient{}): {},
 		//nolint:staticcheck // it is here so the list is complete.
 		sdk.MsgTypeURL(&ibcclienttypes.MsgSubmitMisbehaviour{}): {},
+		sdk.MsgTypeURL(&ibcclienttypes.MsgIBCSoftwareUpgrade{}): {},
+		sdk.MsgTypeURL(&ibcclienttypes.MsgRecoverClient{}):      {},
+		sdk.MsgTypeURL(&ibcclienttypes.MsgUpdateParams{}):       {},
+
+		// ibc/apps/transfer
+		sdk.MsgTypeURL(&ibctransfertypes.MsgUpdateParams{}): {},
 
 		// ibc/core/connection
 		sdk.MsgTypeURL(&ibcconnectiontypes.MsgConnectionOpenInit{}):    {},
 		sdk.MsgTypeURL(&ibcconnectiontypes.MsgConnectionOpenTry{}):     {},
 		sdk.MsgTypeURL(&ibcconnectiontypes.MsgConnectionOpenAck{}):     {},
 		sdk.MsgTypeURL(&ibcconnectiontypes.MsgConnectionOpenConfirm{}): {},
+		sdk.MsgTypeURL(&ibcconnectiontypes.MsgUpdateParams{}):          {},
 
 		// ibc/core/channel
-		sdk.MsgTypeURL(&ibcchanneltypes.MsgChannelOpenInit{}):     {},
-		sdk.MsgTypeURL(&ibcchanneltypes.MsgChannelOpenTry{}):      {},
-		sdk.MsgTypeURL(&ibcchanneltypes.MsgChannelOpenAck{}):      {},
-		sdk.MsgTypeURL(&ibcchanneltypes.MsgChannelOpenConfirm{}):  {},
-		sdk.MsgTypeURL(&ibcchanneltypes.MsgChannelCloseInit{}):    {},
-		sdk.MsgTypeURL(&ibcchanneltypes.MsgChannelCloseConfirm{}): {},
-		sdk.MsgTypeURL(&ibcchanneltypes.MsgRecvPacket{}):          {},
-		sdk.MsgTypeURL(&ibcchanneltypes.MsgTimeout{}):             {},
-		sdk.MsgTypeURL(&ibcchanneltypes.MsgTimeoutOnClose{}):      {},
-		sdk.MsgTypeURL(&ibcchanneltypes.MsgAcknowledgement{}):     {},
+		sdk.MsgTypeURL(&ibcchanneltypes.MsgChannelOpenInit{}):       {},
+		sdk.MsgTypeURL(&ibcchanneltypes.MsgChannelOpenTry{}):        {},
+		sdk.MsgTypeURL(&ibcchanneltypes.MsgChannelOpenAck{}):        {},
+		sdk.MsgTypeURL(&ibcchanneltypes.MsgChannelOpenConfirm{}):    {},
+		sdk.MsgTypeURL(&ibcchanneltypes.MsgChannelCloseInit{}):      {},
+		sdk.MsgTypeURL(&ibcchanneltypes.MsgChannelCloseConfirm{}):   {},
+		sdk.MsgTypeURL(&ibcchanneltypes.MsgRecvPacket{}):            {},
+		sdk.MsgTypeURL(&ibcchanneltypes.MsgTimeout{}):               {},
+		sdk.MsgTypeURL(&ibcchanneltypes.MsgTimeoutOnClose{}):        {},
+		sdk.MsgTypeURL(&ibcchanneltypes.MsgAcknowledgement{}):       {},
+		sdk.MsgTypeURL(&ibcchanneltypes.MsgChannelUpgradeAck{}):     {},
+		sdk.MsgTypeURL(&ibcchanneltypes.MsgChannelUpgradeCancel{}):  {},
+		sdk.MsgTypeURL(&ibcchanneltypes.MsgChannelUpgradeConfirm{}): {},
+		sdk.MsgTypeURL(&ibcchanneltypes.MsgChannelUpgradeInit{}):    {},
+		sdk.MsgTypeURL(&ibcchanneltypes.MsgChannelUpgradeOpen{}):    {},
+		sdk.MsgTypeURL(&ibcchanneltypes.MsgChannelUpgradeTimeout{}): {},
+		sdk.MsgTypeURL(&ibcchanneltypes.MsgChannelUpgradeTry{}):     {},
+		sdk.MsgTypeURL(&ibcchanneltypes.MsgPruneAcknowledgements{}): {},
+		sdk.MsgTypeURL(&ibcchanneltypes.MsgUpdateParams{}):          {},
 
-		// ibc/applications/interchain_accounts
-		sdk.MsgTypeURL(&ibcinterchainaccountstypes.MsgRegisterInterchainAccount{}): {},
-		sdk.MsgTypeURL(&ibcinterchainaccountstypes.MsgSendTx{}):                    {},
+		// ibc/applications/interchain_accounts/controller
+		sdk.MsgTypeURL(&ibcinterchainaccountscontrollertypes.MsgRegisterInterchainAccount{}): {},
+		sdk.MsgTypeURL(&ibcinterchainaccountscontrollertypes.MsgSendTx{}):                    {},
+		sdk.MsgTypeURL(&ibcinterchainaccountscontrollertypes.MsgUpdateParams{}):              {},
+
+		// ibc/applications/interchain_accounts/host
+		sdk.MsgTypeURL(&ibcinterchainaccountshosttypes.MsgModuleQuerySafe{}): {},
+		sdk.MsgTypeURL(&ibcinterchainaccountshosttypes.MsgUpdateParams{}):    {},
 
 		// internal cosmos
 		sdk.MsgTypeURL(&sdktestdatatypes.MsgCreateDog{}): {},
 		sdk.MsgTypeURL(&sdktxtypes.Tx{}):                 {},
 
-		// wasm
-		sdk.MsgTypeURL(&wasmtypes.MsgUpdateContractLabel{}): {},
+		// feegrant
+		sdk.MsgTypeURL(&feegrant.MsgPruneAllowances{}): {},
 	}
 
 	for protoType := range revProtoTypes {
-		sdkMsg, isSDKMessage := reflect.New(protoType.Elem()).Interface().(sdk.Msg)
+		protoInterface := reflect.New(protoType.Elem()).Interface()
+
+		sdkMsg, isSDKMessage := protoInterface.(sdk.Msg)
 		if !isSDKMessage {
 			continue
 		}
 
 		messageURL := sdk.MsgTypeURL(sdkMsg)
-		_, isLegacyMessage := reflect.New(protoType.Elem()).Interface().(legacytx.LegacyMsg)
-
+		_, isLegacyMessage := protoInterface.(legacytx.LegacyMsg)
 		if isLegacyMessage {
+			continue
+		}
+
+		options := proto2.MessageV2(protoInterface).ProtoReflect().Descriptor().Options()
+
+		signersFields := proto.GetExtension(options, msgv1.E_Signer).([]string)
+		if len(signersFields) == 0 && messageURL != "/cosmos.tx.v1beta1.Tx" {
+			continue
+		}
+
+		aminoNameField := proto.GetExtension(options, amino.E_Name).(string)
+		if len(aminoNameField) > 0 {
 			continue
 		}
 
