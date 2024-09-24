1fa270499afb67d730e4213db605beeb4ee92d58
Dzmitry Hil <dzmitryhil@gmail.com>
2024-09-24 14:04:46 +0300

    Fix merge conflict

--- x/dex/genesis_test.go
+++ x/dex/genesis_test.go
@@ -41,7 +41,7 @@ func TestInitAndExportGenesis(t *testing.T) {
 		InitialAmount: sdkmath.NewInt(100),
 	}
 	denom3, err := testApp.AssetFTKeeper.Issue(sdkCtx, settings)
-	require.NoError(t, err)
+	requireT.NoError(err)
 
 	prams := types.DefaultParams()
 	genState := types.GenesisState{
