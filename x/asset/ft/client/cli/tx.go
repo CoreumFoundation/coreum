3effe8c19cc14b7ad3306185a0fcab03ed125f2f
Masih Yeganeh <goodboy.php@gmail.com>
2024-10-14 13:37:29 +0330

    Add time-in-force & fix some cli params (#993)
    * Add time-in-force & fix some cli params
    
    * Address review comments
    
    * Fix placeOrder helper function
    
    * Fix linting
    
    * Merge branch 'master' into masih/time-in-force-cli

--- x/asset/ft/client/cli/tx.go
+++ x/asset/ft/client/cli/tx.go
@@ -33,11 +33,11 @@ const (
 	ExpirationFlag           = "expiration"
 	RecipientFlag            = "recipient"
 	URIFlag                  = "uri"
-	URIHashFlag              = "uri_hash"
-	ExtensionCodeIDFlag      = "extension_code_id"
-	ExtensionLabelFlag       = "extension_label"
-	ExtensionFundsFlag       = "extension_funds"
-	ExtensionIssuanceMsgFlag = "extension_issuance_msg"
+	URIHashFlag              = "uri-hash"
+	ExtensionCodeIDFlag      = "extension-code-id"
+	ExtensionLabelFlag       = "extension-label"
+	ExtensionFundsFlag       = "extension-funds"
+	ExtensionIssuanceMsgFlag = "extension-issuance-msg"
 	DEXUnifiedRefAmountFlag  = "dex-unified-ref-amount"
 )
 
@@ -83,7 +83,7 @@ func CmdTxIssue() *cobra.Command {
 	sort.Strings(allowedFeatures)
 	cmd := &cobra.Command{
 		//nolint:lll // breaking this down will make it look worse when printed to user screen.
-		Use:   fmt.Sprintf("issue [symbol] [subunit] [precision] [initial_amount] [description] --from [issuer] --features="+strings.Join(allowedFeatures, ",")+" --burn-rate=0.12 --send-commission-rate=0.2 --uri https://my-token-meta.invalid/1 --uri_hash e000624 --extension_code_id=1 --extension_label=my-extension --extension_funds=100000ABC-%s --extension_instantiation_msg={}", constant.AddressSampleTest),
+		Use:   fmt.Sprintf("issue [symbol] [subunit] [precision] [initial_amount] [description] --from [issuer] --features="+strings.Join(allowedFeatures, ",")+" --burn-rate=0.12 --send-commission-rate=0.2 --uri https://my-token-meta.invalid/1 --uri_hash e000624 --extension-code-id=1 --extension-label=my-extension --extension-funds=100000ABC-%s --extension-instantiation-msg={}", constant.AddressSampleTest),
 		Args:  cobra.ExactArgs(5),
 		Short: "Issue new fungible token",
 		Long: strings.TrimSpace(
