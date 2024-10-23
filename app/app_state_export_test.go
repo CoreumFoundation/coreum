0777c29fb671aa83752a278ff4a5abb572d7d24a
milad <mza1989@gmail.com>
2024-10-23 16:02:51 +0330

    added test for app export (#1007)

---
+++ app/app_state_export_test.go
@@ -0,0 +1,20 @@
+package app_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/CoreumFoundation/coreum/v5/testutil/simapp"
+)
+
+func TestAppStateExport(t *testing.T) {
+	simApp := simapp.New()
+	require.NoError(t, simApp.FinalizeBlock())
+
+	exportedApp, err := simApp.ExportAppStateAndValidators(
+		false, nil, nil,
+	)
+	require.NoError(t, err)
+	require.NotNil(t, exportedApp.AppState)
+}
