975cc06952d26e7827ea90b673c38cdaee57c16a
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2025-02-06 16:15:08 +0100

    Move accNumberToAddrCache to cachedAccountKeeper abstraction

--- x/dex/keeper/keeper_account.go
+++ x/dex/keeper/keeper_account.go
@@ -2,10 +2,8 @@ package keeper
 
 import (
 	sdkerrors "cosmossdk.io/errors"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-
 	"github.com/CoreumFoundation/coreum/v5/x/dex/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k Keeper) getAccountNumber(ctx sdk.Context, addr sdk.AccAddress) (uint64, error) {
@@ -15,38 +13,4 @@ func (k Keeper) getAccountNumber(ctx sdk.Context, addr sdk.AccAddress) (uint64, 
 	}
 
 	return acc.GetAccountNumber(), nil
-}
-
-func (k Keeper) getAccountAddress(ctx sdk.Context, accNumber uint64) (sdk.AccAddress, error) {
-	addr, err := k.accountQueryServer.AccountAddressByID(
-		ctx,
-		&authtypes.QueryAccountAddressByIDRequest{AccountId: accNumber},
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	acc, err := sdk.AccAddressFromBech32(addr.AccountAddress)
-	if err != nil {
-		return nil, sdkerrors.Wrapf(types.ErrInvalidInput, "invalid address: %s", addr)
-	}
-
-	return acc, nil
-}
-
-func (k Keeper) getAccountAddressWithCache(ctx sdk.Context, accNumber uint64, cache map[uint64]sdk.AccAddress) (
-	sdk.AccAddress,
-	error,
-) {
-	addr, ok := cache[accNumber]
-	if !ok {
-		var err error
-		addr, err = k.getAccountAddress(ctx, accNumber)
-		if err != nil {
-			return nil, err
-		}
-		cache[accNumber] = addr
-	}
-
-	return addr, nil
 }
