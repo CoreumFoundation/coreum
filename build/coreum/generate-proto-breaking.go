643b6d7919116198058fa82801ebf7f2c50b5175
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2025-03-12 21:56:08 +0100

    fix unused

--- build/coreum/generate-proto-breaking.go
+++ build/coreum/generate-proto-breaking.go
@@ -22,10 +22,8 @@ import (
 )
 
 //go:embed proto-breaking.tmpl.json
-//nolint:unused
 var configBreakingTmpl string
 
-//nolint:unused
 func breakingProto(ctx context.Context, deps types.DepsFunc) error {
 	deps(golang.Tidy, coreumtools.EnsureProtoc, coreumtools.EnsureProtocGenBufBreaking)
 
