b4538de881d495cb24ca114964d81d32092949d9
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2025-03-17 21:15:38 +0100

    fix tests & fuzz test

--- x/dex/types/msgs_test.go
+++ x/dex/types/msgs_test.go
@@ -248,7 +248,7 @@ func TestAmino(t *testing.T) {
 				Authority: address,
 				Params:    types.DefaultParams(),
 			},
-			wantAminoJSON: `{"type":"dex/MsgUpdateParams","value":{"authority":"devcore172rc5sz2uclpsy3vvx3y79ah5dk450z5ruq2r5","params":{"default_unified_ref_amount":"1000000.000000000000000000","max_orders_per_denom":"100","order_reserve":{"amount":"10000000","denom":"stake"},"price_tick_exponent":-6}}}`,
+			wantAminoJSON: `{"type":"dex/MsgUpdateParams","value":{"authority":"devcore172rc5sz2uclpsy3vvx3y79ah5dk450z5ruq2r5","params":{"default_unified_ref_amount":"1000000.000000000000000000","max_orders_per_denom":"100","order_reserve":{"amount":"10000000","denom":"stake"},"price_tick_exponent":-6,"quantity_step_exponent":-2}}}`,
 		},
 		{
 			name: sdk.MsgTypeURL(&types.MsgPlaceOrder{}),
