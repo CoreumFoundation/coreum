a49836898ea5ed3fce5ced5d3863b9ae1f5c7723
Masih Yeganeh <goodboy.php@gmail.com>
2025-04-07 10:49:12 +0330

    Make some tests pass for now

--- x/asset/ft/keeper/keeper_dex.go
+++ x/asset/ft/keeper/keeper_dex.go
@@ -34,6 +34,16 @@ type sudoExtensionPlaceOrderMsg struct {
 // adjusting locked balances, and updating expected to receive balances. It performs necessary
 // validations and updates the state accordingly based on the provided actions.
 func (k Keeper) DEXExecuteActions(ctx sdk.Context, actions types.DEXActions) error {
+	// FIXME: This should be last call
+	if err := k.DEXCheckOrderAmounts(
+		ctx,
+		actions.Order,
+		actions.CreatorExpectedToSpend,
+		actions.CreatorExpectedToReceive,
+	); err != nil {
+		return err
+	}
+
 	for _, lock := range actions.IncreaseLocked {
 		if err := k.DEXIncreaseLocked(ctx, lock.Address, lock.Coin); err != nil {
 			return err
@@ -70,12 +80,7 @@ func (k Keeper) DEXExecuteActions(ctx sdk.Context, actions types.DEXActions) err
 		}
 	}
 
-	return k.DEXCheckOrderAmounts(
-		ctx,
-		actions.Order,
-		actions.CreatorExpectedToSpend,
-		actions.CreatorExpectedToReceive,
-	)
+	return nil
 }
 
 // DEXDecreaseLimits decreases the DEX limits.
