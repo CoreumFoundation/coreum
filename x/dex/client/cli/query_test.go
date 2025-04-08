e25436f4926fd119cb7d4c18c402ce45d25470c6
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2025-04-08 18:54:18 +0200

    fix unit test: TestQueryOrderBookParams

--- x/dex/client/cli/query_test.go
+++ x/dex/client/cli/query_test.go
@@ -33,9 +33,11 @@ func TestQueryOrderBookParams(t *testing.T) {
 	testNetwork := network.New(t)
 
 	ctx := testNetwork.Validators[0].ClientCtx
+	denom1 := issueFT(ctx, requireT, testNetwork, sdkmath.NewInt(1_000_000))
+	denom2 := issueFT(ctx, requireT, testNetwork, sdkmath.NewInt(1_000_000))
 
 	var resp types.QueryOrderBookParamsResponse
-	coreumclitestutil.ExecQueryCmd(t, ctx, cli.CmdQueryOrderBookParams(), []string{"denom1", "denom2"}, &resp)
+	coreumclitestutil.ExecQueryCmd(t, ctx, cli.CmdQueryOrderBookParams(), []string{denom1, denom2}, &resp)
 	requireT.Equal("1e-6", resp.PriceTick.String())
 	requireT.Equal("10000", resp.QuantityStep.String())
 	requireT.Equal("1000000", resp.BaseDenomUnifiedRefAmount.TruncateInt().String())
