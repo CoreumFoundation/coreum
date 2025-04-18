6e273cf2698c9dc9697e1b6af6de8342d67965c5
Masih Yeganeh <goodboy.php@gmail.com>
2025-04-18 12:21:48 +0330

    Fix upgrades

--- integration-tests/upgrade/upgrade_test.go
+++ integration-tests/upgrade/upgrade_test.go
@@ -154,7 +154,7 @@ func runUpgrade(
 	//nolint:staticcheck
 	retryCtx, cancel := context.WithTimeout(
 		ctx,
-		10*time.Second*time.Duration(upgradeHeight-infoWaitingBlockRes.Block.Header.Height)+3*time.Minute,
+		6*time.Second*time.Duration(upgradeHeight-infoWaitingBlockRes.Block.Header.Height),
 	)
 	defer cancel()
 	//nolint:staticcheck
